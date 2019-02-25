fbf6d7160840bff705d89dfb79250082d82cd7c0
Ramesh Shiraddi <ramesh.shiraddi@nav.no>
2019-02-25 15:20:10 +0100

    PFP-5692 Henter behandling data fra FPSAK - Ny REST klient som henter behandling informasjon fra FPSAK - Ny DTOer for å ta mot data fra FPSAK hentBehandling tjeneste - Konfig verdier for å koble til FPSAK

---
+++ domenetjenester/fpsak/src/main/java/no/nav/foreldrepenger/fpsak/BehandlingRestKlientImpl.java
@@ -0,0 +1,50 @@
+package no.nav.foreldrepenger.fpsak;
+
+import java.net.URISyntaxException;
+import java.util.Optional;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.apache.http.client.utils.URIBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.foreldrepenger.fpsak.dto.behandling.BehandlingDto;
+import no.nav.foreldrepenger.fpsak.dto.behandling.BehandlingIdDto;
+import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
+import no.nav.vedtak.konfig.KonfigVerdi;
+
+@ApplicationScoped
+public class BehandlingRestKlientImpl implements BehandlingRestKlient {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BehandlingRestKlientImpl.class);
+    private static final String FPSAK_REST_BASE_URL = "fpsak_rest_base.url";
+    private static final String HENT_BEHANLDING_ENDPOINT = "/behandlinger";
+
+    private OidcRestClient oidcRestClient;
+    private String endpointFpsakRestBase;
+
+    public BehandlingRestKlientImpl() {//For CDI
+    }
+
+    @Inject
+    public BehandlingRestKlientImpl(OidcRestClient oidcRestClient,
+                                    @KonfigVerdi(FPSAK_REST_BASE_URL) String endpointFpsakRestBase) {
+        this.oidcRestClient = oidcRestClient;
+        this.endpointFpsakRestBase = endpointFpsakRestBase;
+    }
+
+    @Override
+    public Optional<BehandlingDto> hentBehandling(BehandlingIdDto behandlingIdDto) {
+        Optional<BehandlingDto> behandling = Optional.empty();
+        try {
+            URIBuilder uriBuilder = new URIBuilder(endpointFpsakRestBase + HENT_BEHANLDING_ENDPOINT);
+            uriBuilder.setParameter("behandlingId", String.valueOf(behandlingIdDto.getBehandlingId()));
+
+            behandling = oidcRestClient.getReturnsOptional(uriBuilder.build(), BehandlingDto.class);
+        } catch (URISyntaxException e) {
+            LOGGER.error("Feil ved oppretting av URI.", e);
+        }
+        return behandling;
+    }
+}
--- web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/app/tjenester/hendelse/HendelseRestTjeneste.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/app/tjenester/hendelse/HendelseRestTjeneste.java
@@ -3,12 +3,18 @@ package no.nav.foreldrepenger.melding.web.app.tjenester.hendelse;
 import static no.nav.vedtak.sikkerhet.abac.BeskyttetRessursActionAttributt.READ;
 import static no.nav.vedtak.sikkerhet.abac.BeskyttetRessursResourceAttributt.FAGSAK;
 
-import javax.enterprise.context.RequestScoped;
+import java.util.Optional;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,29 +24,48 @@ import com.codahale.metrics.annotation.Timed;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import no.nav.foreldrepenger.fpsak.BehandlingRestKlient;
+import no.nav.foreldrepenger.fpsak.dto.behandling.BehandlingDto;
+import no.nav.foreldrepenger.fpsak.dto.behandling.BehandlingIdDto;
 import no.nav.foreldrepenger.melding.web.app.tjenester.dto.HendelseDto;
 import no.nav.vedtak.felles.jpa.Transaction;
 import no.nav.vedtak.sikkerhet.abac.BeskyttetRessurs;
 
 @Api(tags = "hendelse")
 @Path("/hendelse")
-@RequestScoped
+@ApplicationScoped
 @Transaction
 public class HendelseRestTjeneste {
-    private static final Logger log = LoggerFactory.getLogger(HendelseRestTjeneste.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(HendelseRestTjeneste.class);
+
+    private BehandlingRestKlient behandlingRestKlient;
 
     public HendelseRestTjeneste() {
         //For Rest-CDI
+    }
+
+    @Inject
+    public HendelseRestTjeneste(BehandlingRestKlient behandlingRestKlient) {
+        this.behandlingRestKlient = behandlingRestKlient;
     }
 
     @POST
     @Timed
     @Path("/prossesere-hendelse")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "Proseserer hendelse og sender ")
     @BeskyttetRessurs(action = READ, ressurs = FAGSAK)
     @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
-    public void prossesereHendelse(@ApiParam("hendelseDto") @Valid HendelseDto hendelseDto) {
-        log.info("hendelse = " + hendelseDto);
+    public Response prossesereHendelse(@NotNull @ApiParam("hendelseDto") @Valid HendelseDto hendelseDto) {
+        LOGGER.info("hendelse = " + hendelseDto);
+        final Optional<BehandlingDto> behandlingInfo = behandlingRestKlient.hentBehandling(new BehandlingIdDto(hendelseDto.getBehandlingId()));
+        Response.ResponseBuilder responseBuilder;
+        if (behandlingInfo.isPresent()) {
+            responseBuilder = Response.ok().entity(behandlingInfo.get());
+        } else {
+            responseBuilder = Response.noContent();
+        }
+        return responseBuilder.build();
     }
 }

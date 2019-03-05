d9240ae4da33c53c673df9bf0f7bb7ed7b5375ac
Ramesh Shiraddi <ramesh.shiraddi@nav.no>
2019-03-05 23:00:03 +0100

    PFP-5810 Felttefelter og relatert hjelp klasser

--- domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/brevbestiller/api/dto/Behandling.java
+++ domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/brevbestiller/api/dto/Behandling.java
@@ -3,9 +3,25 @@ package no.nav.foreldrepenger.melding.brevbestiller.api.dto;
 import no.nav.foreldrepenger.fpsak.dto.behandling.BehandlingDto;
 
 public class Behandling {
+    private String ansvarligSaksbehandler;
+    private String ansvarligBeslutter;
+    private Boolean toTrinnsBehandling;
 
-    public Behandling(BehandlingDto Dto) {
-
+    public Behandling(BehandlingDto dto) {
+        this.ansvarligSaksbehandler = dto.getAnsvarligSaksbehandler();
+//        this.ansvarligBeslutter = ansvarligBeslutter;
+        this.toTrinnsBehandling = dto.getToTrinnsBehandling();
     }
 
+    public String getAnsvarligSaksbehandler() {
+        return ansvarligSaksbehandler;
+    }
+
+    public String getAnsvarligBeslutter() {
+        return ansvarligBeslutter;
+    }
+
+    public Boolean getToTrinnsBehandling() {
+        return toTrinnsBehandling;
+    }
 }

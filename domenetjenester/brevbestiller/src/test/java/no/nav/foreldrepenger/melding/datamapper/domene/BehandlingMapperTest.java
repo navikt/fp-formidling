704912f5348d269b41075289a434180f7ad8fa6a
Aleksander Andresen <caeses@gmail.com>
2019-04-08 19:14:55 +0200

    Oppdatert fritekst (#67)

---
+++ domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/datamapper/domene/BehandlingMapperTest.java
@@ -0,0 +1,65 @@
+package no.nav.foreldrepenger.melding.datamapper.domene;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import no.nav.foreldrepenger.melding.behandling.Behandling;
+import no.nav.foreldrepenger.melding.behandling.Behandlingsresultat;
+import no.nav.foreldrepenger.melding.fagsak.FagsakYtelseType;
+import no.nav.foreldrepenger.melding.hendelser.DokumentHendelse;
+
+public class BehandlingMapperTest {
+
+    private static final String HENDELSE_FRITEKST = "HENDELSE_FRITEKST";
+    private static final String BEHANDLING_FRITEKST = "BEHANDLING_FRITEKST";
+    private static final long BEHANDLING_ID = 123L;
+    Behandling behandling;
+    DokumentHendelse dokumentHendelse;
+
+    @Test
+    public void skal_velge_ingen_fritekst_når_ingen_finnes() {
+        behandling = standardBehandlingBuilder()
+                .build();
+        dokumentHendelse = standardHendelseBuilder().build();
+        assertThat(BehandlingMapper.avklarFritekst(dokumentHendelse, behandling)).isNotPresent();
+    }
+
+    @Test
+    public void skal_prioritere_hendelse() {
+        behandling = standardBehandlingBuilder()
+                .medBehandlingsresultat(Behandlingsresultat.ny()
+                        .medAvslagarsakFritekst(BEHANDLING_FRITEKST)
+                        .build())
+                .build();
+        dokumentHendelse = standardHendelseBuilder()
+                .medFritekst(HENDELSE_FRITEKST)
+                .build();
+        assertThat(BehandlingMapper.avklarFritekst(dokumentHendelse, behandling).get()).isEqualTo(HENDELSE_FRITEKST);
+    }
+
+
+    @Test
+    public void skal_ta_fritekst_fra_behandling_når_mangler_i_hendelse() {
+        behandling = standardBehandlingBuilder()
+                .medBehandlingsresultat(Behandlingsresultat.ny()
+                        .medAvslagarsakFritekst(BEHANDLING_FRITEKST)
+                        .build())
+                .build();
+        DokumentHendelse dokumentHendelse = standardHendelseBuilder()
+                .build();
+        assertThat(BehandlingMapper.avklarFritekst(dokumentHendelse, behandling).get()).isEqualTo(BEHANDLING_FRITEKST);
+    }
+
+    private DokumentHendelse.Builder standardHendelseBuilder() {
+        return DokumentHendelse.builder()
+                .medBehandlingId(BEHANDLING_ID)
+                .medYtelseType(FagsakYtelseType.FORELDREPENGER);
+    }
+
+    private Behandling.Builder standardBehandlingBuilder() {
+        return Behandling.builder()
+                .medId(BEHANDLING_ID);
+    }
+
+}

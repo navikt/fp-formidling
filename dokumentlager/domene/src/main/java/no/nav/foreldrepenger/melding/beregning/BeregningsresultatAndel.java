b30edef9b3e46ce1d182956a5f8bc3b21741b28a
Aleksander Andresen <caeses@gmail.com>
2019-04-23 15:28:41 +0200

    PFP-7214 Begynner å mappe beregningsresultat fra dto

--- dokumentlager/domene/src/main/java/no/nav/foreldrepenger/melding/beregning/BeregningsresultatAndel.java
+++ dokumentlager/domene/src/main/java/no/nav/foreldrepenger/melding/beregning/BeregningsresultatAndel.java
@@ -2,16 +2,30 @@ package no.nav.foreldrepenger.melding.beregning;
 
 import java.util.Optional;
 
+import no.nav.foreldrepenger.melding.beregningsgrunnlag.AktivitetStatus;
 import no.nav.foreldrepenger.melding.typer.ArbeidsforholdRef;
 import no.nav.foreldrepenger.melding.virksomhet.Arbeidsgiver;
 
 public class BeregningsresultatAndel {
     private int dagsats;
     private String stillingsprosent;
-    private String aktivitetStatus; //Kode
+    private AktivitetStatus aktivitetStatus;
     private Arbeidsgiver arbeidsgiver;
     private ArbeidsforholdRef arbeidsforholdRef;
     private Boolean brukerErMottaker;
+
+    private BeregningsresultatAndel(Builder builder) {
+        dagsats = builder.dagsats;
+        stillingsprosent = builder.stillingsprosent;
+        aktivitetStatus = builder.aktivitetStatus;
+        arbeidsgiver = builder.arbeidsgiver;
+        arbeidsforholdRef = builder.arbeidsforholdRef;
+        brukerErMottaker = builder.brukerErMottaker;
+    }
+
+    public static Builder ny() {
+        return new Builder();
+    }
 
     public int getDagsats() {
         return dagsats;
@@ -21,7 +35,7 @@ public class BeregningsresultatAndel {
         return stillingsprosent;
     }
 
-    public String getAktivitetStatus() {
+    public AktivitetStatus getAktivitetStatus() {
         return aktivitetStatus;
     }
 
@@ -35,5 +49,52 @@ public class BeregningsresultatAndel {
 
     public boolean erBrukerMottaker() {
         return brukerErMottaker;
+    }
+
+
+    public static final class Builder {
+        private int dagsats;
+        private String stillingsprosent;
+        private AktivitetStatus aktivitetStatus;
+        private Arbeidsgiver arbeidsgiver;
+        private ArbeidsforholdRef arbeidsforholdRef;
+        private Boolean brukerErMottaker;
+
+        private Builder() {
+        }
+
+        public Builder medDagsats(int dagsats) {
+            this.dagsats = dagsats;
+            return this;
+        }
+
+        public Builder medStillingsprosent(String stillingsprosent) {
+            this.stillingsprosent = stillingsprosent;
+            return this;
+        }
+
+        public Builder medAktivitetStatus(AktivitetStatus aktivitetStatus) {
+            this.aktivitetStatus = aktivitetStatus;
+            return this;
+        }
+
+        public Builder medArbeidsgiver(Arbeidsgiver arbeidsgiver) {
+            this.arbeidsgiver = arbeidsgiver;
+            return this;
+        }
+
+        public Builder medArbeidsforholdRef(ArbeidsforholdRef arbeidsforholdRef) {
+            this.arbeidsforholdRef = arbeidsforholdRef;
+            return this;
+        }
+
+        public Builder medBrukerErMottaker(Boolean brukerErMottaker) {
+            this.brukerErMottaker = brukerErMottaker;
+            return this;
+        }
+
+        public BeregningsresultatAndel build() {
+            return new BeregningsresultatAndel(this);
+        }
     }
 }

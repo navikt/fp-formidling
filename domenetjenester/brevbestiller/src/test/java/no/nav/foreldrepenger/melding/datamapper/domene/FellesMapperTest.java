91a7e42214d9141b789887cb5ffddedbfba27f4a
Aleksander Andresen <caeses@gmail.com>
2019-04-26 14:38:32 +0200

    Kopierer over tester for lovhjemmelformatering

--- domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/datamapper/domene/FellesMapperTest.java
+++ domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/datamapper/domene/FellesMapperTest.java
@@ -3,6 +3,8 @@ package no.nav.foreldrepenger.melding.datamapper.domene;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.Test;
 
@@ -28,6 +30,35 @@ public class FellesMapperTest {
         String lovhjemmelFraBeregning = "folketrygdloven § 14-7";
         assertLovformateringBeregning(lovhjemmelFraBeregning, KonsekvensForYtelseKode.ENDRING_I_BEREGNING.value(), false, "§ 14-7 og forvaltningsloven § 35");
     }
+
+
+    @Test
+    public void skal_formatere_lovhjemmel_uttak_med_forvaltningsloven() {
+        Set<String> hjemmelSet = new TreeSet<>();
+        hjemmelSet.add("14-16");
+        hjemmelSet.add("14-18");
+        String resultat = FellesMapper.formaterLovhjemlerUttak(hjemmelSet, "", true);
+        assertThat(resultat).isEqualTo("§§ 14-16, 14-18 og forvaltningsloven § 35");
+    }
+
+    @Test
+    public void skal_formatere_lovhjemmel_uttak() {
+        Set<String> hjemmelSet = new TreeSet<>();
+        hjemmelSet.add("14-16");
+        hjemmelSet.add("14-18");
+        hjemmelSet.add("14-17");
+        String resultat = FellesMapper.formaterLovhjemlerUttak(hjemmelSet, "", false);
+        assertThat(resultat).isEqualTo("§§ 14-16, 14-17 og 14-18");
+    }
+
+    @Test
+    public void skal_formatere_enkel_lovhjemmel() {
+        Set<String> hjemmelSet = new TreeSet<>();
+        hjemmelSet.add("14-16");
+        String resultat = FellesMapper.formaterLovhjemlerUttak(hjemmelSet, "", false);
+        assertThat(resultat).isEqualTo("§ 14-16");
+    }
+
 
     private void assertLovformateringBeregning(String input, String konsekvensForYtelse, boolean innvilgetRevurdering, String forventetOutput) {
         String lovhjemler = FellesMapper.formaterLovhjemlerForBeregning(input, konsekvensForYtelse, innvilgetRevurdering);

bb9d0b5c6e3696163e90f314ad250e837f3c001f
Aleksander Andresen <caeses@gmail.com>
2019-04-29 09:16:42 +0200

    Oppdaterer søknad med søknadsdato (#84)

--- integrasjon/dtomapper/src/main/java/no/nav/foreldrepenger/melding/dtomapper/SøknadDtoMapper.java
+++ integrasjon/dtomapper/src/main/java/no/nav/foreldrepenger/melding/dtomapper/SøknadDtoMapper.java
@@ -4,9 +4,7 @@ import no.nav.foreldrepenger.fpsak.dto.soknad.SoknadDto;
 import no.nav.foreldrepenger.melding.søknad.Søknad;
 
 public class SøknadDtoMapper {
-
     public static Søknad mapSøknadFraDto(SoknadDto dto) {
-        // TODO venter på søknadsdato
-        return new Søknad(dto.getMottattDato());
+        return new Søknad(dto.getMottattDato(), dto.getSoknadsdato());
     }
 }

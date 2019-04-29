6773b2636f51e822ac39e2fc8965f497e83ba52a
tnarland <47184872+tnarland@users.noreply.github.com>
2019-04-29 08:57:33 +0200

    PFP-6849 Fpformidling: Mappe brev om Avslått foreldrepenger (#83)
    - Mapper avslagsårsaker

---
+++ domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/datamapper/domene/AvslagsårsakMapper.java
@@ -0,0 +1,94 @@
+package no.nav.foreldrepenger.melding.datamapper.domene;
+
+import static no.nav.foreldrepenger.melding.datamapper.domene.sammenslåperioder.PeriodeBeregner.alleAktiviteterHarNullUtbetaling;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import no.nav.foreldrepenger.melding.behandling.Behandlingsresultat;
+import no.nav.foreldrepenger.melding.beregning.BeregningsresultatPeriode;
+import no.nav.foreldrepenger.melding.brevbestiller.XmlUtil;
+import no.nav.foreldrepenger.melding.datamapper.domene.sammenslåperioder.PeriodeBeregner;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.avslag.foreldrepenger.AarsakListeType;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.avslag.foreldrepenger.AvslagsAarsakType;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.avslag.foreldrepenger.ObjectFactory;
+import no.nav.foreldrepenger.melding.uttak.PeriodeResultatType;
+import no.nav.foreldrepenger.melding.uttak.PeriodeResultatÅrsak;
+import no.nav.foreldrepenger.melding.uttak.UttakResultatPeriode;
+import no.nav.foreldrepenger.melding.uttak.UttakResultatPeriodeAktivitet;
+import no.nav.foreldrepenger.melding.uttak.UttakResultatPerioder;
+import no.nav.foreldrepenger.melding.vilkår.Avslagsårsak;
+
+@ApplicationScoped
+public class AvslagsårsakMapper {
+
+    private static ObjectFactory objectFactory = new ObjectFactory();
+
+    public static AarsakListeType mapAarsakListeFra(Behandlingsresultat behandlingsresultat,
+                                                    List<BeregningsresultatPeriode> beregningsresultatPerioder,
+                                                    UttakResultatPerioder uttakResultatPerioder) {
+        AarsakListeType aarsakListeType = objectFactory.createAarsakListeType();
+        aarsakListeType.getAvslagsAarsak().addAll(årsakerFra(beregningsresultatPerioder, uttakResultatPerioder));
+        if (aarsakListeType.getAvslagsAarsak().isEmpty()) {
+            aarsakListeType.getAvslagsAarsak().addAll(årsakerFra(behandlingsresultat, uttakResultatPerioder));
+        }
+        return aarsakListeType;
+    }
+
+    private static Set<AvslagsAarsakType> årsakerFra(List<BeregningsresultatPeriode> beregningsresultatPerioder,
+                                                     UttakResultatPerioder uttakResultatPerioder) {
+        Set<AvslagsAarsakType> avslagsAarsaker = new HashSet<>();
+        for (BeregningsresultatPeriode beregningsresultatPeriode : beregningsresultatPerioder) {
+            AvslagsAarsakType aarsakType = årsaktypeFra(beregningsresultatPeriode,
+                    PeriodeBeregner.finnUttaksPeriode(beregningsresultatPeriode, uttakResultatPerioder.getPerioder()));
+            avslagsAarsaker.add(aarsakType);
+        }
+        return avslagsAarsaker;
+    }
+
+    private static Set<AvslagsAarsakType> årsakerFra(Behandlingsresultat behandlingsresultat,
+                                                     UttakResultatPerioder uttakResultatPerioder) {
+        Set<AvslagsAarsakType> avslagsAarsaker = new HashSet<>();
+
+        Avslagsårsak avslagsårsak = behandlingsresultat.getAvslagsårsak();
+        if (avslagsårsak != null) {
+            avslagsAarsaker.add(årsaktypeFra(avslagsårsak.getKode()));
+        }
+        for (UttakResultatPeriode periode : uttakResultatPerioder.getPerioder()) {
+            PeriodeResultatÅrsak periodeResultatÅrsak = periode.getPeriodeResultatÅrsak();
+            if (PeriodeResultatType.AVSLÅTT.equals(periode.getPeriodeResultatType()) && periodeResultatÅrsak != null) {
+                String årsakKode = periodeResultatÅrsak.getKode();
+                avslagsAarsaker.add(årsaktypeFra(årsakKode));
+            }
+        }
+        return avslagsAarsaker;
+    }
+
+    private static AvslagsAarsakType årsaktypeFra(String årsakKode) {
+        AvslagsAarsakType avslagsAarsak = objectFactory.createAvslagsAarsakType();
+        avslagsAarsak.setAvslagsAarsakKode(årsakKode);
+        return avslagsAarsak;
+    }
+
+    static AvslagsAarsakType årsaktypeFra(BeregningsresultatPeriode beregningsresultatPeriode,
+                                          UttakResultatPeriode uttakResultatPeriode) {
+        AvslagsAarsakType periode = årsaktypeFra(uttakResultatPeriode.getPeriodeResultatÅrsak().getKode());
+        periode.setAntallTapteDager(BigInteger.valueOf(mapAntallTapteDagerFra(uttakResultatPeriode.getAktiviteter())));
+        periode.setPeriodeFom(XmlUtil.finnDatoVerdiAvUtenTidSone(beregningsresultatPeriode.getBeregningsresultatPeriodeFom()));
+        periode.setPeriodeTom(XmlUtil.finnDatoVerdiAvUtenTidSone(beregningsresultatPeriode.getBeregningsresultatPeriodeTom()));
+
+        return periode;
+    }
+
+    private static int mapAntallTapteDagerFra(List<UttakResultatPeriodeAktivitet> uttakAktiviteter) {
+        return alleAktiviteterHarNullUtbetaling(uttakAktiviteter) ?
+                uttakAktiviteter.stream()
+                        .mapToInt(UttakResultatPeriodeAktivitet::getTrekkdager)
+                        .max()
+                        .orElse(0) : 0;
+    }
+}

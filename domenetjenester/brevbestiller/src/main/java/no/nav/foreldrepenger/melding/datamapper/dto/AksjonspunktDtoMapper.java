3dc7feddf3d4d43725d74b06be9257e2c7745d7d
Ramesh Shiraddi <extrsi@users.noreply.github.com>
2019-05-03 11:09:47 +0200

    PFP-7193 Hente liste av aksjonspunker fra fpsak (#97)

--- domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/datamapper/dto/AksjonspunktDtoMapper.java
+++ domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/datamapper/dto/AksjonspunktDtoMapper.java
@@ -6,16 +6,15 @@ import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 
 import no.nav.foreldrepenger.fpsak.dto.behandling.aksjonspunkt.AksjonspunktDto;
-import no.nav.foreldrepenger.fpsak.dto.behandling.aksjonspunkt.AksjonspunkterDto;
 import no.nav.foreldrepenger.melding.aksjonspunkt.Aksjonspunkt;
 import no.nav.foreldrepenger.melding.aksjonspunkt.AksjonspunktDefinisjon;
 
 @ApplicationScoped
 public class AksjonspunktDtoMapper {
 
-    public List<Aksjonspunkt> mapAksjonspunktFraDto(AksjonspunkterDto dto) {
+    public List<Aksjonspunkt> mapAksjonspunktFraDto(List<AksjonspunktDto> dtoer) {
         List<Aksjonspunkt> aksjonspunktList = new ArrayList<>();
-        for (AksjonspunktDto aksjonspunktDto : dto.getAksjonspunktDtos()) {
+        for (AksjonspunktDto aksjonspunktDto : dtoer) {
             aksjonspunktList.add(Aksjonspunkt.ny()
                     .medAksjonspunktDefinisjon(finnAksjonspunktDefinisjon(aksjonspunktDto.getDefinisjon().getKode()))
                     .build());

0cd59eb2dd7c618a62e583489eee0e0be3f18ad4
Aleksander Andresen <caeses@gmail.com>
2019-05-03 14:56:34 +0200

    Slår sammen perioder og arbeidsforhold for innvilgelse

--- domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/datamapper/domene/sammenslåperioder/PeriodeMergerInnvilgelse.java
+++ domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/datamapper/domene/sammenslåperioder/PeriodeMergerInnvilgelse.java
@@ -8,31 +8,31 @@ import static no.nav.foreldrepenger.melding.datamapper.domene.sammenslåperioder
 import java.util.ArrayList;
 import java.util.List;
 
-import no.nav.foreldrepenger.melding.datamapper.domene.hjelperdto.PeriodeDto;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.PeriodeType;
 
-public class PeriodeMerger {
+public class PeriodeMergerInnvilgelse {
 
     static List<String> nonEqualKoderSomLikevelOppfyllerMerge =
             List.of("2001", "2002", "2003", "2004", "2005", "2006", "2007");
 
-    private PeriodeMerger() {
+    private PeriodeMergerInnvilgelse() {
     }
 
-    public static List<PeriodeDto> mergePerioder(List<PeriodeDto> perioder) {
+    public static List<PeriodeType> mergePerioder(List<PeriodeType> perioder) {
         if (perioder.size() <= 1) {
             return perioder; // ikke noe å se på.
         }
         return slåSammenSammenhengendePerioder(perioder);
     }
 
-    private static List<PeriodeDto> slåSammenSammenhengendePerioder(List<PeriodeDto> perioder) {
-        List<PeriodeDto> resultat = new ArrayList<>();
+    private static List<PeriodeType> slåSammenSammenhengendePerioder(List<PeriodeType> perioder) {
+        List<PeriodeType> resultat = new ArrayList<>();
         for (int index = 0; index < perioder.size() - 1; index++) {
             boolean sistePeriode = (index == perioder.size() - 2);
-            PeriodeDto periodeEn = perioder.get(index);
-            PeriodeDto periodeTo = perioder.get(index + 1);
+            PeriodeType periodeEn = perioder.get(index);
+            PeriodeType periodeTo = perioder.get(index + 1);
             if (erPerioderSammenhengendeOgSkalSlåSammen(periodeEn, periodeTo)) {
-                PeriodeDto nyPeriode = slåSammenPerioder(periodeEn, periodeTo);
+                PeriodeType nyPeriode = slåSammenPerioder(periodeEn, periodeTo);
                 perioder.set(index + 1, nyPeriode);
                 if (sistePeriode) {
                     resultat.add(nyPeriode);
@@ -47,7 +47,7 @@ public class PeriodeMerger {
         return resultat;
     }
 
-    private static boolean erPerioderSammenhengendeOgSkalSlåSammen(PeriodeDto periodeEn, PeriodeDto periodeTo) {
+    private static boolean erPerioderSammenhengendeOgSkalSlåSammen(PeriodeType periodeEn, PeriodeType periodeTo) {
         return sammeStatusOgÅrsak(periodeEn, periodeTo) && likeAktiviteter(periodeEn, periodeTo) && erFomRettEtterTomDato(periodeEn, periodeTo);
     }
 }
---
+++ domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/datamapper/domene/sammenslåperioder/PeriodeMergerVerktøyTest.java
@@ -0,0 +1,135 @@
+package no.nav.foreldrepenger.melding.datamapper.domene.sammenslåperioder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.AnnenAktivitetListeType;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.AnnenAktivitetType;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.ArbeidsforholdListeType;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.ArbeidsforholdType;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.NæringListeType;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.NæringType;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.ObjectFactory;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.PeriodeType;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.innvilget.foreldrepenger.StatusTypeKode;
+
+public class PeriodeMergerVerktøyTest {
+
+    ObjectFactory objectFactory = new ObjectFactory();
+    PeriodeType periodeEn = objectFactory.createPeriodeType();
+    PeriodeType periodeTo = objectFactory.createPeriodeType();
+
+    @Test
+    public void skal_finne_like_aktiviteter_når_ikke_satt() {
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isTrue();
+    }
+
+    @Test
+    public void skal_finne_at_aktiviteter_er_ulike() {
+        periodeTo.setNæringListe(objectFactory.createNæringListeType());
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+        periodeTo = objectFactory.createPeriodeType();
+        periodeTo.setArbeidsforholdListe(objectFactory.createArbeidsforholdListeType());
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+        periodeTo = objectFactory.createPeriodeType();
+        periodeTo.setAnnenAktivitetListe(objectFactory.createAnnenAktivitetListeType());
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+    }
+
+    @Test
+    public void skal_finne_like_aktiviteter_kompleks_næring() {
+        NæringListeType næringListeEn = objectFactory.createNæringListeType();
+        NæringListeType næringListeTo = objectFactory.createNæringListeType();
+
+        periodeEn.setNæringListe(næringListeEn);
+        periodeTo.setNæringListe(næringListeTo);
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isTrue();
+
+        NæringType næringEn = objectFactory.createNæringType();
+        periodeEn.getNæringListe().setNæring(næringEn);
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+
+        NæringType næringTo = objectFactory.createNæringType();
+        næringTo.setInntekt1(200L);
+        periodeTo.getNæringListe().setNæring(næringTo);
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+
+        periodeEn.getNæringListe().getNæring().setInntekt1(200L);
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isTrue();
+
+        periodeEn.getNæringListe().getNæring().setUttaksgrad(BigInteger.ONE);
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isTrue();
+
+        periodeEn.getNæringListe().getNæring().setSistLignedeÅr(BigInteger.valueOf(2019));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+
+        periodeTo.getNæringListe().getNæring().setSistLignedeÅr(BigInteger.valueOf(2019));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isTrue();
+    }
+
+    @Test
+    public void skal_finne_like_aktiviteter_kompleks_annen_aktivitet() {
+        AnnenAktivitetListeType annenAktivitetListeEn = objectFactory.createAnnenAktivitetListeType();
+        AnnenAktivitetListeType annenAktivitetListeTo = objectFactory.createAnnenAktivitetListeType();
+        periodeEn.setAnnenAktivitetListe(annenAktivitetListeEn);
+        periodeTo.setAnnenAktivitetListe(annenAktivitetListeTo);
+
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isTrue();
+
+        periodeEn.getAnnenAktivitetListe().getAnnenAktivitet().add(opprettAnnenAktivitet(100L, null));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+
+        periodeTo.getAnnenAktivitetListe().getAnnenAktivitet().add(opprettAnnenAktivitet(200L, null));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+
+        annenAktivitetListeTo = objectFactory.createAnnenAktivitetListeType();
+        periodeTo.setAnnenAktivitetListe(annenAktivitetListeTo);
+        periodeTo.getAnnenAktivitetListe().getAnnenAktivitet().add(opprettAnnenAktivitet(100L, null));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isTrue();
+
+        periodeTo.getAnnenAktivitetListe().getAnnenAktivitet().add(opprettAnnenAktivitet(100L, null));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+
+        periodeEn.getAnnenAktivitetListe().getAnnenAktivitet().add(opprettAnnenAktivitet(100L, StatusTypeKode.ARBEIDSTAKER));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+    }
+
+    @Test
+    public void skal_finne_like_aktiviteter_kompleks_arbeidsforhold() {
+        ArbeidsforholdListeType arbeidsforholdListeEn = objectFactory.createArbeidsforholdListeType();
+        ArbeidsforholdListeType arbeidsforholdListeTo = objectFactory.createArbeidsforholdListeType();
+        periodeEn.setArbeidsforholdListe(arbeidsforholdListeEn);
+        periodeTo.setArbeidsforholdListe(arbeidsforholdListeTo);
+
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isTrue();
+
+        periodeEn.getArbeidsforholdListe().getArbeidsforhold().add(opprettArbeidsforhold(false, 100L, null));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+
+        periodeTo.getArbeidsforholdListe().getArbeidsforhold().add(opprettArbeidsforhold(true, 100L, null));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isTrue();
+
+        periodeTo.getArbeidsforholdListe().getArbeidsforhold().add(opprettArbeidsforhold(false, 200L, null));
+        periodeEn.getArbeidsforholdListe().getArbeidsforhold().add(opprettArbeidsforhold(false, 200L, 200L));
+        assertThat(PeriodeMergerVerktøy.likeAktiviteter(periodeEn, periodeTo)).isFalse();
+    }
+
+    private ArbeidsforholdType opprettArbeidsforhold(boolean gradering, long dagsats, Long naturalytelseNyDagsats) {
+        ArbeidsforholdType arbeidsforhold = objectFactory.createArbeidsforholdType();
+        arbeidsforhold.setAktivitetDagsats(dagsats);
+        arbeidsforhold.setGradering(gradering);
+        arbeidsforhold.setNaturalytelseNyDagsats(naturalytelseNyDagsats);
+        return arbeidsforhold;
+    }
+
+    private AnnenAktivitetType opprettAnnenAktivitet(long dagsats, StatusTypeKode aktivitetType) {
+        AnnenAktivitetType annenAktivitet = objectFactory.createAnnenAktivitetType();
+        annenAktivitet.setAktivitetDagsats(dagsats);
+        annenAktivitet.setAktivitetType(aktivitetType);
+        return annenAktivitet;
+    }
+
+}

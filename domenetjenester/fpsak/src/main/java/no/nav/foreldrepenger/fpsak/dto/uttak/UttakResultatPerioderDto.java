5d335df45d6523aa1f3828cc07af460e5ff2de34
Aleksander Andresen <caeses@gmail.com>
2019-05-06 10:04:15 +0200

    Legger til default konstruktør i uttaksdto

--- domenetjenester/fpsak/src/main/java/no/nav/foreldrepenger/fpsak/dto/uttak/UttakResultatPerioderDto.java
+++ domenetjenester/fpsak/src/main/java/no/nav/foreldrepenger/fpsak/dto/uttak/UttakResultatPerioderDto.java
@@ -7,11 +7,6 @@ public class UttakResultatPerioderDto {
     private List<UttakResultatPeriodeDto> perioderSøker;
     private List<UttakResultatPeriodeDto> perioderAnnenpart;
 
-    public UttakResultatPerioderDto(List<UttakResultatPeriodeDto> perioderSøker, List<UttakResultatPeriodeDto> perioderAnnenpart) {
-        this.perioderSøker = perioderSøker;
-        this.perioderAnnenpart = perioderAnnenpart;
-    }
-
     public List<UttakResultatPeriodeDto> getPerioderSøker() {
         return perioderSøker;
     }

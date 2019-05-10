09d80c10d9324d83eba8ae2175364584f361d61f
Ramesh Shiraddi <extrsi@users.noreply.github.com>
2019-05-10 16:42:01 +0200

    Feature/resttjenester (#121)
    PFP-7729 Rest tjenester for forhåndsvisning og bestilling av brev

---
+++ domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/brevbestiller/dto/BestillBrevDto.java
@@ -0,0 +1,6 @@
+package no.nav.foreldrepenger.melding.brevbestiller.dto;
+
+import no.nav.foreldrepenger.melding.hendelsekontrakter.hendelse.DokumentHendelseDto;
+
+public class BestillBrevDto extends DokumentHendelseDto {
+}
--- web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/app/tjenester/brev/BrevRestTjeneste.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/app/tjenester/brev/BrevRestTjeneste.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.melding.web.app.tjenester.brev;
 
 import static no.nav.vedtak.sikkerhet.abac.BeskyttetRessursActionAttributt.READ;
+import static no.nav.vedtak.sikkerhet.abac.BeskyttetRessursActionAttributt.UPDATE;
 import static no.nav.vedtak.sikkerhet.abac.BeskyttetRessursResourceAttributt.FAGSAK;
 
 import java.util.List;
@@ -8,20 +9,34 @@ import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.validation.Valid;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.codahale.metrics.annotation.Timed;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import no.nav.foreldrepenger.fpsak.dto.behandling.BehandlingIdDto;
+import no.nav.foreldrepenger.melding.brevbestiller.api.BrevBestillerApplikasjonTjeneste;
 import no.nav.foreldrepenger.melding.brevbestiller.api.DokumentBehandlingTjeneste;
+import no.nav.foreldrepenger.melding.brevbestiller.dto.BestillBrevDto;
 import no.nav.foreldrepenger.melding.brevbestiller.dto.BrevmalDto;
+import no.nav.foreldrepenger.melding.brevbestiller.task.ProduserBrevTaskProperties;
 import no.nav.foreldrepenger.melding.dokumentdata.DokumentMalType;
+import no.nav.foreldrepenger.melding.dtomapper.DokumentHendelseDtoMapper;
+import no.nav.foreldrepenger.melding.hendelser.DokumentHendelse;
+import no.nav.foreldrepenger.melding.hendelser.HendelseRepository;
 import no.nav.vedtak.felles.jpa.Transaction;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskRepository;
 import no.nav.vedtak.sikkerhet.abac.BeskyttetRessurs;
 
 @Api(tags = "brev")
@@ -29,15 +44,28 @@ import no.nav.vedtak.sikkerhet.abac.BeskyttetRessurs;
 @ApplicationScoped
 @Transaction
 public class BrevRestTjeneste {
+    private static final Logger LOG = LoggerFactory.getLogger(BrevRestTjeneste.class);
     private DokumentBehandlingTjeneste dokumentBehandlingTjeneste;
+    private BrevBestillerApplikasjonTjeneste brevBestillerApplikasjonTjeneste;
+    private ProsessTaskRepository prosessTaskRepository;
+    private DokumentHendelseDtoMapper dtoTilDomeneobjektMapper;
+    private HendelseRepository hendelseRepository;
 
     public BrevRestTjeneste() {
         //CDI
     }
 
     @Inject
-    public BrevRestTjeneste(DokumentBehandlingTjeneste dokumentBehandlingTjeneste) {
+    public BrevRestTjeneste(DokumentBehandlingTjeneste dokumentBehandlingTjeneste,
+                            BrevBestillerApplikasjonTjeneste brevBestillerApplikasjonTjeneste,
+                            ProsessTaskRepository prosessTaskRepository,
+                            DokumentHendelseDtoMapper dtoTilDomeneobjektMapper,
+                            HendelseRepository hendelseRepository) {
         this.dokumentBehandlingTjeneste = dokumentBehandlingTjeneste;
+        this.brevBestillerApplikasjonTjeneste = brevBestillerApplikasjonTjeneste;
+        this.prosessTaskRepository = prosessTaskRepository;
+        this.dtoTilDomeneobjektMapper = dtoTilDomeneobjektMapper;
+        this.hendelseRepository = hendelseRepository;
     }
 
     @POST
@@ -60,5 +88,48 @@ public class BrevRestTjeneste {
     @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
     public Boolean harSendtVarselOmRevurdering(@Valid BehandlingIdDto dto) {
         return dokumentBehandlingTjeneste.erDokumentProdusert(dto.getBehandlingId(), DokumentMalType.REVURDERING_DOK); // NOSONAR
+    }
+
+    @POST
+    @Timed
+    @Path("/forhandsvis")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Returnerer en pdf som er en forhåndsvisning av brevet")
+    @BeskyttetRessurs(action = READ, ressurs = FAGSAK)
+    @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
+    public Response hentForhåndsvisningDokument(
+            @ApiParam("Inneholder kode til brevmal og data som skal flettes inn i brevet") @Valid BestillBrevDto bestillBrevDto) { // NOSONAR
+        Response.ResponseBuilder responseBuilder;
+        byte[] dokument = brevBestillerApplikasjonTjeneste.forhandsvisBrev(bestillBrevDto);
+        if (dokument != null && dokument.length != 0) {
+            responseBuilder = Response.ok().entity(java.util.Base64.getEncoder().encode(dokument));
+            responseBuilder.type("application/pdf");
+            responseBuilder.header("Content-Disposition", "filename=dokument.pdf");
+            return responseBuilder.build();
+        }
+        responseBuilder = Response.serverError();
+        return responseBuilder.build();
+    }
+
+    @POST
+    @Timed
+    @Path("/bestill")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Bestiller generering og sending av brevet")
+    @BeskyttetRessurs(action = UPDATE, ressurs = FAGSAK)
+    @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
+    public void bestillDokument(
+            @ApiParam("Inneholder kode til brevmal og data som skal flettes inn i brevet") @Valid BestillBrevDto bestillBrevDto) { // NOSONAR
+        DokumentHendelse hendelse = dtoTilDomeneobjektMapper.mapDokumentHendelseFraDto(bestillBrevDto);
+        hendelseRepository.lagre(hendelse);
+        opprettBestillBrevTask(hendelse);
+        LOG.info("lagret hendelse:{} for behandling: {} OK", hendelse.getId(), hendelse.getBehandlingUuid());
+    }
+
+    private void opprettBestillBrevTask(DokumentHendelse dokumentHendelse) {
+        ProsessTaskData prosessTaskData = new ProsessTaskData(ProduserBrevTaskProperties.TASKTYPE);
+        prosessTaskData.setProperty(ProduserBrevTaskProperties.HENDELSE_ID, String.valueOf(dokumentHendelse.getId()));
+        prosessTaskData.setGruppe("FORMIDLING");
+        prosessTaskRepository.lagre(prosessTaskData);
     }
 }

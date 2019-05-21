398da88b3afa35ef6c6df8547e7a17c6fea5d2fc
Aleksander Andresen <caeses@gmail.com>
2019-05-21 07:26:56 +0200

    Setter eldre flyway versjon og slår av clean av databasen

--- web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/server/jetty/db/DatabaseScript.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/server/jetty/db/DatabaseScript.java
@@ -9,7 +9,6 @@ import org.flywaydb.core.api.configuration.ClassicConfiguration;
 public class DatabaseScript {
 
     private static final String location = "classpath:/db/migration/";
-    private static final boolean SKAL_CLEANE_DATABASEN = true;
 
     public static void migrate(final DataSource dataSource, String initSql) {
         ClassicConfiguration conf = new ClassicConfiguration();
@@ -23,12 +22,7 @@ public class DatabaseScript {
         try {
             flyway.migrate();
         } catch (FlywayException fwe) {
-            if (SKAL_CLEANE_DATABASEN) {
-                flyway.clean();
-                flyway.migrate();
-            } else {
                 throw new IllegalStateException("Migrering feiler", fwe);
-            }
         }
     }
 }

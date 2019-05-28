df6574a72b818e9781741b25c8d722876a3448e2
Aleksander Andresen <caeses@gmail.com>
2019-05-28 11:12:54 +0200

    Sikkerhetsoppsett som virker

--- web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/app/pdp/PdpRequestBuilderImpl.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/app/pdp/PdpRequestBuilderImpl.java
@@ -1,7 +1,9 @@
-package no.nav.foreldrepenger.melding.web.server.jetty;
+package no.nav.foreldrepenger.melding.web.app.pdp;
+
+import java.util.Collections;
 
 import javax.annotation.Priority;
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Alternative;
 
 import no.nav.abac.xacml.NavAttributter;
@@ -10,12 +12,11 @@ import no.nav.vedtak.sikkerhet.abac.AbacAttributtSamling;
 import no.nav.vedtak.sikkerhet.abac.PdpKlient;
 import no.nav.vedtak.sikkerhet.abac.PdpRequest;
 import no.nav.vedtak.sikkerhet.abac.PdpRequestBuilder;
-import no.nav.vedtak.sikkerhet.abac.StandardAbacAttributtType;
 
 /**
  * Implementasjon av PDP request for denne applikasjonen.
  */
-@ApplicationScoped
+@Dependent
 @Alternative
 @Priority(2)
 // HACK - FORDI DENNE KOLLIDERER MED DUMMYREQUESTBUILDER FRA FELLES-SIKKERHET-TESTUTILITIES NÅR VI KJØRER JETTY
@@ -27,11 +28,10 @@ public class PdpRequestBuilderImpl implements PdpRequestBuilder {
     public PdpRequest lagPdpRequest(AbacAttributtSamling attributter) {
         PdpRequest pdpRequest = new PdpRequest();
         pdpRequest.put(PdpKlient.ENVIRONMENT_AUTH_TOKEN, attributter.getIdToken());
-        pdpRequest.put(StandardAttributter.ACTION_ID, attributter.getActionType());
+        pdpRequest.put(StandardAttributter.ACTION_ID, attributter.getActionType().getEksternKode());
         pdpRequest.put(NavAttributter.RESOURCE_FELLES_DOMENE, ABAC_DOMAIN);
-        pdpRequest.put(NavAttributter.RESOURCE_FELLES_RESOURCE_TYPE, attributter.getResource());
-        pdpRequest.put(NavAttributter.RESOURCE_FELLES_PERSON_AKTOERID_RESOURCE, attributter.getVerdier(StandardAbacAttributtType.AKTØR_ID));
-        pdpRequest.put(NavAttributter.RESOURCE_FELLES_PERSON_FNR, attributter.getVerdier(StandardAbacAttributtType.FNR));
+        pdpRequest.put(NavAttributter.RESOURCE_FELLES_RESOURCE_TYPE, attributter.getResource().getEksternKode());
+        pdpRequest.put(NavAttributter.RESOURCE_FELLES_PERSON_AKTOERID_RESOURCE, Collections.emptyList());
         return pdpRequest;
     }
 }

8ac601be268ff81daa47043f3ed3499d36733216
Aleksander Andresen <caeses@gmail.com>
2019-05-28 11:12:54 +0200

    Sikkerhetsoppsett som virker

--- domenetjenester/fpsak/src/main/java/no/nav/foreldrepenger/fpsak/dto/behandling/BehandlingIdDto.java
+++ domenetjenester/fpsak/src/main/java/no/nav/foreldrepenger/fpsak/dto/behandling/BehandlingIdDto.java
@@ -9,8 +9,10 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import no.nav.vedtak.sikkerhet.abac.AbacAttributtType;
 import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
 import no.nav.vedtak.sikkerhet.abac.AbacDto;
+import no.nav.vedtak.sikkerhet.abac.StandardAbacAttributtType;
 
 /**
  * Referanse til en behandling.
@@ -70,7 +72,7 @@ public class BehandlingIdDto implements AbacDto {
     @Override
     public AbacDataAttributter abacAttributter() {
         AbacDataAttributter abac = AbacDataAttributter.opprett();
-        return abac.leggTilBehandlingsId(getBehandlingId());
+        return abac.leggTil(StandardAbacAttributtType.BEHANDLING_UUID,getBehandlingId());
     }
 
     @Override

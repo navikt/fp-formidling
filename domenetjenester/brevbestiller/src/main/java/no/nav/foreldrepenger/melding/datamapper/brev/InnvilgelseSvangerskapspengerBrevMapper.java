1d5f98c161e0b2ed3d9808a6774b58a65af9141f
Tor Garmann Nærland <Tor.Nerland@nav.no>
2019-05-28 17:53:17 +0200

    NOJIRA EtterlysInntektsmeldingBrev:
    - Henter søknadsdato fra MottattdokumentMapper
    - CDI workaround

--- domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/datamapper/brev/InnvilgelseSvangerskapspengerBrevMapper.java
+++ domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/datamapper/brev/InnvilgelseSvangerskapspengerBrevMapper.java
@@ -1,15 +1,26 @@
 package no.nav.foreldrepenger.melding.datamapper.brev;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Named;
+import javax.inject.Inject;
 
 import no.nav.foreldrepenger.melding.behandling.Behandling;
-import no.nav.foreldrepenger.melding.dokumentdata.DokumentMalType;
+import no.nav.foreldrepenger.melding.datamapper.DomeneobjektProvider;
+import no.nav.foreldrepenger.melding.datamapper.konfig.BrevParametere;
 import no.nav.foreldrepenger.melding.hendelser.DokumentHendelse;
 
 @ApplicationScoped
 //@Named(DokumentMalType.INNVILGELSE_SVANGERSKAPSPENGER_DOK) //TODO
 public class InnvilgelseSvangerskapspengerBrevMapper extends FritekstmalBrevMapper {
+
+    public InnvilgelseSvangerskapspengerBrevMapper() {
+        //CDI
+    }
+
+    @Inject
+    public InnvilgelseSvangerskapspengerBrevMapper(BrevParametere brevParametere, DomeneobjektProvider domeneobjektProvider) {
+        super(brevParametere, domeneobjektProvider);
+    }
+
     @Override
     public String displayName() {
         return null;

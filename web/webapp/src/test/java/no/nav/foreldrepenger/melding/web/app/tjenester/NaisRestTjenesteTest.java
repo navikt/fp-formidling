08426590feecb728614ca212fa2862116f0235ca
Aleksander Andresen <caeses@gmail.com>
2019-06-04 15:31:43 +0200

    Feiler liveness probe hvis kritisk selftest feiler (#147)

--- web/webapp/src/test/java/no/nav/foreldrepenger/melding/web/app/tjenester/NaisRestTjenesteTest.java
+++ web/webapp/src/test/java/no/nav/foreldrepenger/melding/web/app/tjenester/NaisRestTjenesteTest.java
@@ -3,21 +3,26 @@ package no.nav.foreldrepenger.melding.web.app.tjenester;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import javax.ws.rs.core.Response;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import no.nav.foreldrepenger.melding.web.app.selftest.SelftestService;
+
 public class NaisRestTjenesteTest {
 
     private NaisRestTjeneste restTjeneste;
 
+    private SelftestService selftestServiceMock = mock(SelftestService.class);
     private ApplicationServiceStarter serviceStarterMock = mock(ApplicationServiceStarter.class);
 
     @Before
     public void setup() {
-        restTjeneste = new NaisRestTjeneste(serviceStarterMock);
+        when(selftestServiceMock.kritiskTjenesteFeilet()).thenReturn(false);
+        restTjeneste = new NaisRestTjeneste(serviceStarterMock, selftestServiceMock);
     }
 
     @Test

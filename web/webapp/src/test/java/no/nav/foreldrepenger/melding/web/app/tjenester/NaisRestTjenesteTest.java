694fd9c59f7d740b52569be1c30ba898d94b1af6
Aleksander Andresen <caeses@gmail.com>
2019-06-19 12:44:27 +0200

    Fikser test

--- web/webapp/src/test/java/no/nav/foreldrepenger/melding/web/app/tjenester/NaisRestTjenesteTest.java
+++ web/webapp/src/test/java/no/nav/foreldrepenger/melding/web/app/tjenester/NaisRestTjenesteTest.java
@@ -3,26 +3,21 @@ package no.nav.foreldrepenger.melding.web.app.tjenester;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import javax.ws.rs.core.Response;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import no.nav.foreldrepenger.melding.web.app.selftest.SelftestService;
-
 public class NaisRestTjenesteTest {
 
     private NaisRestTjeneste restTjeneste;
 
-    private SelftestService selftestServiceMock = mock(SelftestService.class);
     private ApplicationServiceStarter serviceStarterMock = mock(ApplicationServiceStarter.class);
 
     @Before
     public void setup() {
-        when(selftestServiceMock.kritiskTjenesteFeilet()).thenReturn(false);
-        restTjeneste = new NaisRestTjeneste(serviceStarterMock, selftestServiceMock);
+        restTjeneste = new NaisRestTjeneste(serviceStarterMock);
     }
 
     @Test

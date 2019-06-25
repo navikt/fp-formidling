8fca834d3e6dc60e450a97c3a7729c62b12820ee
Aleksander Andresen <aleksander.andresen@nav.no>
2019-06-25 14:45:35 +0200

    Avslag relasjons es (#197)
    * Setter relasjonsrolle på avslag av ES likt som i FPSAK
    
    * Legger til test

---
+++ domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/datamapper/brev/AvslagEngangstønadBrevMapperTest.java
@@ -0,0 +1,35 @@
+package no.nav.foreldrepenger.melding.datamapper.brev;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.Test;
+
+import no.nav.foreldrepenger.melding.aktør.NavBrukerKjønn;
+import no.nav.foreldrepenger.melding.integrasjon.dokument.avslag.RelasjonskodeType;
+import no.nav.foreldrepenger.melding.personopplysning.RelasjonsRolleType;
+
+public class AvslagEngangstønadBrevMapperTest {
+
+    @Test
+    public void skal_mappe_relasjonsrolle_far() {
+        assertThat(AvslagEngangstønadBrevMapper
+                .tilRelasjonskodeType(RelasjonsRolleType.REGISTRERT_PARTNER, NavBrukerKjønn.MANN))
+                .isEqualTo(RelasjonskodeType.FAR);
+    }
+
+
+    @Test
+    public void skal_mappe_relasjonsrolle_mor() {
+        assertThat(AvslagEngangstønadBrevMapper
+                .tilRelasjonskodeType(RelasjonsRolleType.MORA, NavBrukerKjønn.KVINNE))
+                .isEqualTo(RelasjonskodeType.MOR);
+    }
+
+    @Test
+    public void skal_mappe_relasjonsrolle_medmor() {
+        assertThat(AvslagEngangstønadBrevMapper
+                .tilRelasjonskodeType(RelasjonsRolleType.SAMBOER, NavBrukerKjønn.KVINNE))
+                .isEqualTo(RelasjonskodeType.MEDMOR);
+    }
+
+}

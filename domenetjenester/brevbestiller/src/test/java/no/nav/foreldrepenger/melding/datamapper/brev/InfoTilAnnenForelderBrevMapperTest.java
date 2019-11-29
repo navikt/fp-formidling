40656e442e25e56ad9ee0407e938cf40bef079e3
AnjaAalerud <52454508+AnjaAalerud@users.noreply.github.com>
2019-11-29 11:12:04 +0100

    TFP-1816 Lagt til test av INAFOR brevet når det trigges pga oppholdsperioder det ikke er søkt om (#290)

--- domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/datamapper/brev/InfoTilAnnenForelderBrevMapperTest.java
+++ domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/datamapper/brev/InfoTilAnnenForelderBrevMapperTest.java
@@ -3,11 +3,11 @@ package no.nav.foreldrepenger.melding.datamapper.brev;
 import static no.nav.foreldrepenger.melding.datamapper.mal.fritekst.BrevmalKilder.ROTMAPPE;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.UUID;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -18,6 +18,8 @@ import org.mockito.junit.MockitoRule;
 
 import no.nav.foreldrepenger.melding.behandling.Behandling;
 import no.nav.foreldrepenger.melding.behandling.BehandlingType;
+import no.nav.foreldrepenger.melding.behandling.BehandlingÅrsak;
+import no.nav.foreldrepenger.melding.behandling.BehandlingÅrsakType;
 import no.nav.foreldrepenger.melding.datamapper.konfig.BrevParametere;
 import no.nav.foreldrepenger.melding.dokumentdata.DokumentFelles;
 import no.nav.foreldrepenger.melding.fagsak.FagsakYtelseType;
@@ -27,7 +29,7 @@ import no.nav.foreldrepenger.melding.integrasjon.dokument.felles.FellesType;
 import no.nav.foreldrepenger.melding.integrasjon.dokument.fritekstbrev.FagType;
 
 public class InfoTilAnnenForelderBrevMapperTest {
-    private static final long ID = 123L;
+    private long ID;
     private Behandling behandling;
     private DokumentHendelse dokumentHendelse;
 
@@ -43,12 +45,13 @@ public class InfoTilAnnenForelderBrevMapperTest {
     @InjectMocks
     private InfoTilAnnenForelderBrevMapper mapper;
 
-    @Before
-    public void setup() {
-
+    public void setup(String behandlingÅrsakType, long ID) {
         behandling = Behandling.builder().medId(ID)
                 .medBehandlingType(BehandlingType.FØRSTEGANGSSØKNAD)
                 .medSpråkkode(Språkkode.nb)
+                .medBehandlingÅrsaker(List.of(BehandlingÅrsak.builder()
+                    .medBehandlingÅrsakType(behandlingÅrsakType)
+                    .build()))
                 .build();
         dokumentHendelse = DokumentHendelse.builder()
                 .medBehandlingUuid(UUID.randomUUID())
@@ -63,7 +66,8 @@ public class InfoTilAnnenForelderBrevMapperTest {
                 return new Brevdata()
                         .leggTil("kontaktTelefonnummer", null)
                         .leggTil("navnAvsenderEnhet", "Avsender")
-                        .leggTil("erAutomatiskVedtak", true);
+                        .leggTil("erAutomatiskVedtak", true)
+                        .leggTil("behandlingsAarsak", behandling.getBehandlingÅrsaker().get(0).getBehandlingÅrsakType());
             }
         };
         MockitoAnnotations.initMocks(this);
@@ -71,12 +75,27 @@ public class InfoTilAnnenForelderBrevMapperTest {
 
     @Test
     public void test_map_fagtype() {
+        setup(BehandlingÅrsakType.INFOBREV_BEHANDLING.getKode(), 123L);
+
         ResourceBundle expectedValues = ResourceBundle.getBundle(
                 String.join("/", ROTMAPPE, mapper.templateFolder(), "expected"),
                 new Locale("nb", "NO"));
 
         FagType fagType = mapper.mapFagType(dokumentHendelse, behandling);
         assertThat(fagType.getBrødtekst()).isEqualToNormalizingNewlines(expectedValues.getString("brødtekst"));
+        assertThat(fagType.getHovedoverskrift()).isEqualToIgnoringWhitespace(expectedValues.getString("overskrift"));
+    }
+
+    @Test
+    public void test_map_fagtypeOpphold() {
+      setup(BehandlingÅrsakType.INFOBREV_OPPHOLD_BEHANDLING.getKode(), 124L);
+
+      ResourceBundle expectedValues = ResourceBundle.getBundle(
+            String.join("/", ROTMAPPE, mapper.templateFolder(), "expected"),
+            new Locale("nb", "NO"));
+
+        FagType fagType = mapper.mapFagType(dokumentHendelse, behandling);
+        assertThat(fagType.getBrødtekst()).isEqualToNormalizingNewlines(expectedValues.getString("brødtekstOpphold"));
         assertThat(fagType.getHovedoverskrift()).isEqualToIgnoringWhitespace(expectedValues.getString("overskrift"));
     }
 }

37f2588bbda5614c5990693ffeb15c1ddc6ebd53
Jan Erik Johnsen <jejohnsen@users.noreply.github.com>
2020-04-23 14:03:40 +0200

    TFP-2516: Implementert utestående på fritekstversjonen av brev for oversendelse av klage til NAV Klageinstans, inkludert støtte for tilbakekreving. (#492)

--- domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/datamapper/brev/KlageOversendtTilKlageinstansBrevMapper.java
+++ domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/datamapper/brev/KlageOversendtTilKlageinstansBrevMapper.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.melding.datamapper.brev;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -8,7 +9,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import no.nav.foreldrepenger.melding.behandling.Behandling;
-import no.nav.foreldrepenger.melding.datamapper.BrevMapperUtil;
 import no.nav.foreldrepenger.melding.datamapper.DomeneobjektProvider;
 import no.nav.foreldrepenger.melding.datamapper.konfig.BrevParametere;
 import no.nav.foreldrepenger.melding.dokumentdata.DokumentMalType;
@@ -17,6 +17,7 @@ import no.nav.foreldrepenger.melding.integrasjon.dokument.fritekstbrev.FagType;
 import no.nav.foreldrepenger.melding.klage.Klage;
 import no.nav.foreldrepenger.melding.klage.KlageDokument;
 import no.nav.foreldrepenger.melding.typer.Dato;
+
 @ApplicationScoped
 @Named(DokumentMalType.KLAGE_OVERSENDT_KLAGEINSTANS)
 public class KlageOversendtTilKlageinstansBrevMapper extends FritekstmalBrevMapper {
@@ -43,27 +44,46 @@ public class KlageOversendtTilKlageinstansBrevMapper extends FritekstmalBrevMapp
     }
 
     @Override
-    Brevdata mapTilBrevfelter(DokumentHendelse hendelse, Behandling behandling) {
+    protected FagType mapFagType(DokumentHendelse hendelse, Behandling behandling) {
+        initHandlebars(behandling.getSpråkkode());
 
+        Map<String, Object> hovedoverskriftFelter = new HashMap<>();
+        hovedoverskriftFelter.put("behandling", behandling);
+        hovedoverskriftFelter.put("dokumentHendelse", hendelse);
+
+        Map<String, Object> brødtekstFelter = mapTilBrevfelter(hendelse, behandling).getMap();
+
+        Klage klage = domeneobjektProvider.hentKlagebehandling(behandling);
+        if (klage != null) {
+            brødtekstFelter.put("paaklagdBehandlingErTilbakekreving", klage.getPåklagdBehandlingType().erTilbakekrevingBehandlingType());
+        }
+
+        FagType fagType = new FagType();
+        fagType.setHovedoverskrift(tryApply(hovedoverskriftFelter, getOverskriftMal()));
+        fagType.setBrødtekst(tryApply(brødtekstFelter, getBrødtekstMal()));
+        return fagType;
+    }
+
+    @Override
+    Brevdata mapTilBrevfelter(DokumentHendelse hendelse, Behandling behandling) {
         KlageDokument klageDokument = domeneobjektProvider.hentKlageDokument(behandling);
         Klage klage = domeneobjektProvider.hentKlagebehandling(behandling);
 
-        final LocalDate mottatDato = utledMottattDato(klageDokument, behandling);
+        LocalDate mottatDato = utledMottattDato(klageDokument, behandling);
 
-
-        return new Brevdata()
+        Brevdata brevdata = new Brevdata()
                 .leggTil("mottatDato", Dato.formaterDato(mottatDato))
-                .leggTil("mintekst",  klage.getGjeldendeKlageVurderingsresultat().getFritekstTilBrev())
                 .leggTil("behandlingsfrist", BEHANDLINGSFRIST_UKER_KA)
-                .leggTil("saksbehandler", behandling.getAnsvarligSaksbehandler())
-                .leggTil("medunderskriver",behandling.getAnsvarligBeslutter())
-                .leggTil("dokumentHendelseYtelseTypeKode",hendelse.getYtelseType().getKode())
-                .leggTil("behandlingtype",behandling.getBehandlingType().getKode());
+                .leggTil("dokumentHendelseYtelseTypeKode", hendelse.getYtelseType().getKode());
 
-
-
-
+        if (hendelse.getFritekst() != null) { // Forhåndsvisning
+            brevdata.leggTil("mintekst", hendelse.getFritekst());
+        } else if (klage.getGjeldendeKlageVurderingsresultat() != null) { // Bestilling
+            brevdata.leggTil("mintekst", klage.getGjeldendeKlageVurderingsresultat().getFritekstTilBrev());
+        }
+        return brevdata;
     }
+
     private LocalDate utledMottattDato(KlageDokument klageDokument, Behandling behandling) {
         return klageDokument.getMottattDato() != null
                 ? klageDokument.getMottattDato()

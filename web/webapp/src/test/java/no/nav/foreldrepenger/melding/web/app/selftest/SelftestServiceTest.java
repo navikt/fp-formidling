23bb340831ae9f1565940d404f71c8a9fd86ebb3
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-04-27 08:24:38 +0200

    fix: forover-kompat jaxrs

--- web/webapp/src/test/java/no/nav/foreldrepenger/melding/web/app/selftest/SelftestServiceTest.java
+++ web/webapp/src/test/java/no/nav/foreldrepenger/melding/web/app/selftest/SelftestServiceTest.java
@@ -89,8 +89,8 @@ public class SelftestServiceTest {
         Response response = service.doSelftest(TEXT_HTML, false);
 
         assertThat(response).isNotNull();
-        assertThat(response.getHeaders()).containsKeys("Content-Type");
-        assertThat(response.getHeaders().get("Content-Type")).contains(TEXT_HTML);
+        assertThat(response.getStringHeaders()).containsKeys("Content-Type");
+        assertThat(response.getStringHeaders().get("Content-Type")).contains(TEXT_HTML);
         assertThat(response.getEntity()).isNotNull();
     }
 
@@ -102,8 +102,8 @@ public class SelftestServiceTest {
         Response response = service.doSelftest(TEXT_HTML, true);
 
         assertThat(response).isNotNull();
-        assertThat(response.getHeaders()).containsKeys("Content-Type");
-        assertThat(response.getHeaders().get("Content-Type")).contains(TEXT_HTML);
+        assertThat(response.getStringHeaders()).containsKeys("Content-Type");
+        assertThat(response.getStringHeaders().get("Content-Type")).contains(TEXT_HTML);
         assertThat(response.getEntity()).isNotNull();
     }
 

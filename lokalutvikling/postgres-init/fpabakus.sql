13e19a839d2aaa4faa7c9792983b39a675674ce5
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2020-06-04 13:05:54 +0200

    Fjerner Docker Compose og dens avhenighgeter. Linker til autotest som gjør dette. (#552)

--- lokalutvikling/postgres-init/fpabakus.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE fpabakus_unit;
-CREATE USER fpabakus_unit WITH PASSWORD 'fpabakus_unit';
-GRANT ALL PRIVILEGES ON DATABASE fpabakus_unit TO fpabakus_unit;
-ALTER DATABASE fpabakus_unit SET timezone TO 'Europe/Oslo';
-
-CREATE DATABASE fpabakus;
-CREATE USER fpabakus WITH PASSWORD 'fpabakus';
-GRANT ALL PRIVILEGES ON DATABASE fpabakus TO fpabakus;
-ALTER DATABASE fpabakus SET timezone TO 'Europe/Oslo';

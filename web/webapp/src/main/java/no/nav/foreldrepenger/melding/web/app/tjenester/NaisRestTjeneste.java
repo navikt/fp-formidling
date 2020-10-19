78a81ed678097e1d5ca2fdb895ea401a3683e152
Jan Erik Johnsen <jan.erik.johnsen@nav.no>
2020-10-19 16:05:21 +0200

    NOJIRA: Tester om et kall til selftest kan hjelpe med å initiere WS-consumer så det ikke feiler i første kall når verdikjedetestene kjører.

--- web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/app/tjenester/NaisRestTjeneste.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/melding/web/app/tjenester/NaisRestTjeneste.java
@@ -6,12 +6,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.swagger.v3.oas.annotations.Operation;
+import no.nav.foreldrepenger.melding.dokumentproduksjon.v2.DokumentproduksjonConsumerProducer;
 import no.nav.foreldrepenger.melding.web.app.selftest.checks.DatabaseHealthCheck;
 
 @Path("/health")
 @ApplicationScoped
 public class NaisRestTjeneste {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NaisRestTjeneste.class);
 
     private static final String RESPONSE_CACHE_KEY = "Cache-Control";
     private static final String RESPONSE_CACHE_VAL = "must-revalidate,no-cache,no-store";
@@ -19,21 +25,34 @@ public class NaisRestTjeneste {
 
     private ApplicationServiceStarter starterService;
     private DatabaseHealthCheck databaseHealthCheck;
+    private DokumentproduksjonConsumerProducer dokumentproduksjonConsumerProducer;
+
+    private boolean harInitDokprod = false;
 
     public NaisRestTjeneste() {
         // CDI
     }
 
     @Inject
-    public NaisRestTjeneste(ApplicationServiceStarter starterService, DatabaseHealthCheck databaseHealthCheck) {
+    public NaisRestTjeneste(ApplicationServiceStarter starterService, DatabaseHealthCheck databaseHealthCheck,
+                            DokumentproduksjonConsumerProducer dokumentproduksjonConsumerProducer) {
         this.starterService = starterService;
         this.databaseHealthCheck = databaseHealthCheck;
+        this.dokumentproduksjonConsumerProducer = dokumentproduksjonConsumerProducer;
     }
 
     @GET
     @Path("isReady")
     @Operation(description = "sjekker om poden er klar", tags = "nais", hidden = true)
     public Response isReady() {
+        if (!harInitDokprod) {
+            try {
+                dokumentproduksjonConsumerProducer.dokumentproduksjonSelftestConsumer().ping();
+            } catch (Exception e) {
+                LOGGER.info("Kall til dokprod-selftest feilet: " + e.getMessage(), e);
+            }
+            harInitDokprod = true;
+        }
         if (starterService.isKafkaAlive() && databaseHealthCheck.isReady()) {
             return Response
                     .ok(RESPONSE_OK)

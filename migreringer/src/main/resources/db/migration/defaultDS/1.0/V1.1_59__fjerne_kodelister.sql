fd8a69374a3e2da403e2275970aeedefbee6d6cd
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2020-11-02 18:38:34 +0100

    NOJIRA slett kodelister ny felles rydde etter enforce (#807)

---
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0/V1.1_59__fjerne_kodelister.sql
@@ -0,0 +1,10 @@
+drop table dokument_mal_type;
+drop table konfig_verdi;
+drop table konfig_verdi_kode;
+drop table kodeliste_relasjon;
+drop table kodeliste;
+drop table kodeverk;
+
+drop sequence seq_kodeliste;
+drop sequence seq_kodeliste_relasjon;
+drop sequence seq_konfig_verdi;

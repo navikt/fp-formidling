cf5fe320ad875efeb061b20139880979755835bb
anjaaalerud <anja.gotesen.aalerud@nav.no>
2020-11-17 09:32:07 +0100

    TFP-3847 Lagt inn avslagsbrev for engangsstønad i fp-dokgen

--- dokumentlager/domene/src/main/java/no/nav/foreldrepenger/melding/vilkår/Avslagsårsak.java
+++ dokumentlager/domene/src/main/java/no/nav/foreldrepenger/melding/vilkår/Avslagsårsak.java
@@ -1,7 +1,11 @@
 package no.nav.foreldrepenger.melding.vilkår;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
@@ -77,6 +81,21 @@ public enum Avslagsårsak implements Kodeverdi, ÅrsakMedLovReferanse {
 
     public static final String KODEVERK = "AVSLAGSARSAK"; //$NON-NLS-1$
 
+    public static final Set<Avslagsårsak> ALLEREDE_UTBETALT_ENGANGSSTØNAD = Collections.unmodifiableSet(new LinkedHashSet<>(
+            Arrays.asList(ENGANGSSTØNAD_ALLEREDE_UTBETALT_TIL_MOR, ENGANGSTØNAD_ER_ALLEREDE_UTBETAL_TIL_MOR,
+                    ENGANGSSTØNAD_ER_ALLEREDE_UTBETALT_TIL_FAR_MEDMOR)));
+
+    public static final Set<Avslagsårsak> ALLEREDE_UTBETALT_FORELDREPENGER = Collections.unmodifiableSet(new LinkedHashSet<>(
+            Arrays.asList(FORELDREPENGER_ER_ALLEREDE_UTBETALT_TIL_MOR, FORELDREPENGER_ER_ALLEREDE_UTBETALT_TIL_FAR_MEDMOR )));
+
+    public static final Set<Avslagsårsak> IKKE_ALENEOMSORG = Collections.unmodifiableSet(new LinkedHashSet<>(
+            Arrays.asList(SØKER_ER_MEDMOR, SØKER_ER_FAR )));
+
+    public static final Set<Avslagsårsak> BARN_IKKE_RIKTIG_ALDER = Collections.unmodifiableSet(new LinkedHashSet<>(
+            Arrays.asList(BARN_OVER_15_ÅR, BARN_IKKE_UNDER_15_ÅR)));
+
+    public static final Set<Avslagsårsak> IKKE_BARNETS_FAR = Collections.unmodifiableSet(new LinkedHashSet<>(
+            Arrays.asList(SØKER_ER_IKKE_BARNETS_FAR_F, SØKER_ER_IKKE_BARNETS_FAR_O)));
 
     // TODO endre fra raw json
     @JsonIgnore
@@ -84,7 +103,7 @@ public enum Avslagsårsak implements Kodeverdi, ÅrsakMedLovReferanse {
 
     private String kode;
 
-    private Avslagsårsak(String kode, String lovReferanse) {
+    Avslagsårsak(String kode, String lovReferanse) {
         this.kode = kode;
         this.lovReferanse = lovReferanse;
     }
@@ -113,12 +132,28 @@ public enum Avslagsårsak implements Kodeverdi, ÅrsakMedLovReferanse {
         return KODEVERK;
     }
 
-
     @Override
     public String getLovHjemmelData() {
         return lovReferanse;
     }
 
+    public static boolean erAlleredeUtbetaltEngangsstønad(Avslagsårsak avslagsårsak) {
+        return ALLEREDE_UTBETALT_ENGANGSSTØNAD.contains(avslagsårsak);
+    }
 
+    public static boolean erAlleredeUtbetaltForeldrepenger(Avslagsårsak avslagsårsak) {
+        return ALLEREDE_UTBETALT_FORELDREPENGER.contains(avslagsårsak);
+    }
 
+    public static boolean farHarIkkeAleneomsorg(Avslagsårsak avslagsårsak) {
+        return IKKE_ALENEOMSORG.contains(avslagsårsak);
+    }
+
+    public static boolean barnIkkeRiktigAlder(Avslagsårsak avslagsårsak) {
+        return BARN_IKKE_RIKTIG_ALDER.contains(avslagsårsak);
+    }
+
+    public static boolean ikkeBarnetsFar(Avslagsårsak avslagsårsak) {
+        return IKKE_BARNETS_FAR.contains(avslagsårsak);
+    }
 }
---
+++ domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/brevmapper/brev/AvslagEngangsstønadDokumentDataMapperTest.java
@@ -0,0 +1,149 @@
+package no.nav.foreldrepenger.melding.brevmapper.brev;
+
+import no.nav.foreldrepenger.melding.aktør.Personinfo;
+import no.nav.foreldrepenger.melding.behandling.Behandling;
+import no.nav.foreldrepenger.melding.behandling.BehandlingType;
+import no.nav.foreldrepenger.melding.behandling.Behandlingsresultat;
+import no.nav.foreldrepenger.melding.datamapper.DatamapperTestUtil;
+import no.nav.foreldrepenger.melding.datamapper.DomeneobjektProvider;
+import no.nav.foreldrepenger.melding.dokumentdata.DokumentFelles;
+import no.nav.foreldrepenger.melding.fagsak.FagsakBackend;
+import no.nav.foreldrepenger.melding.familiehendelse.FamilieHendelse;
+import no.nav.foreldrepenger.melding.familiehendelse.FamilieHendelseType;
+import no.nav.foreldrepenger.melding.hendelser.DokumentHendelse;
+import no.nav.foreldrepenger.melding.integrasjon.dokgen.dto.EngangsstønadAvslagDokumentData;
+import no.nav.foreldrepenger.melding.kodeverk.kodeverdi.BehandlingResultatType;
+import no.nav.foreldrepenger.melding.personopplysning.NavBrukerKjønn;
+import no.nav.foreldrepenger.melding.personopplysning.RelasjonsRolleType;
+import no.nav.foreldrepenger.melding.typer.AktørId;
+import no.nav.foreldrepenger.melding.typer.PersonIdent;
+import no.nav.foreldrepenger.melding.vilkår.Avslagsårsak;
+import no.nav.foreldrepenger.melding.vilkår.Vilkår;
+import no.nav.foreldrepenger.melding.vilkår.VilkårType;
+import no.nav.foreldrepenger.tps.TpsTjeneste;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AvslagEngangsstønadDokumentDataMapperTest {
+    private AvslagEngangsstønadDokumentDataMapper avslagEngangsstønadDokumentDataMapper;
+    private DokumentFelles dokumentFelles;
+    private static final long ID = 123456;
+    private static final AktørId AKTØR_ID = new AktørId("2222222222222");
+    @Mock
+    private DomeneobjektProvider domeneobjektProvider = mock(DomeneobjektProvider.class);
+    @Mock
+    private DokumentHendelse dokumentHendelse = mock(DokumentHendelse.class);
+    @Mock
+    private TpsTjeneste tpsTjeneste = mock(TpsTjeneste.class);
+
+    @BeforeEach
+    void setUp() {
+        avslagEngangsstønadDokumentDataMapper = new AvslagEngangsstønadDokumentDataMapper(DatamapperTestUtil.getBrevParametere(), domeneobjektProvider, tpsTjeneste);
+        dokumentFelles = DatamapperTestUtil.lagStandardDokumentFelles(DatamapperTestUtil.lagDokumentData(), null, null);
+        Personinfo personinfo = new Personinfo.Builder()
+                .medPersonIdent( new PersonIdent("9999999999"))
+                .medAktørId(AKTØR_ID)
+                .medNavn("Nav Navesen")
+                .medNavBrukerKjønn(NavBrukerKjønn.KVINNE)
+                .build();
+        lenient().when(tpsTjeneste.hentBrukerForAktør(any())).thenReturn(Optional.of(personinfo));
+    }
+
+    @Test
+    void mapTilDokumentData_avslag_ESFB_søknadsfrist_med_Fritekst_mappes_ok() {
+        //Arrange
+        FagsakBackend fagsak = opprettFagsak(RelasjonsRolleType.MORA);
+        FamilieHendelse familieHendelse = new FamilieHendelse(BigInteger.ONE, false, true, FamilieHendelseType.TERMIN, new FamilieHendelse.OptionalDatoer(Optional.of(LocalDate.now()), Optional.empty(), Optional.empty(), Optional.empty()));
+        List<Vilkår> vilkårFraBehandling = List.of(new Vilkår(VilkårType.SØKNADSFRISTVILKÅRET));
+
+        String avslagsfritekst = "Vi har ikke motatt informasjon som begrunner at du ikke har kunnet søke i tide. Derfor avslås saken.";
+
+        Behandling avslagESFB = opprettBehandling(Avslagsårsak.SØKT_FOR_SENT, avslagsfritekst, fagsak );
+
+        when(domeneobjektProvider.hentFamiliehendelse(avslagESFB)).thenReturn(familieHendelse);
+        when(domeneobjektProvider.hentVilkår(avslagESFB)).thenReturn(vilkårFraBehandling);
+
+        //Act
+        EngangsstønadAvslagDokumentData avslagDokumentData = avslagEngangsstønadDokumentDataMapper.mapTilDokumentdata(dokumentFelles, dokumentHendelse, avslagESFB);
+
+        //Verify
+        assertThat(avslagDokumentData.getAvslagÅrsak()).isEqualTo(Avslagsårsak.SØKT_FOR_SENT.name());
+        assertThat(avslagDokumentData.getAntallBarn()).isEqualTo(1);
+        assertThat(avslagDokumentData.getFørstegangsbehandling()).isTrue();
+        assertThat(avslagDokumentData.getgjelderFødsel()).isTrue();
+        assertThat(avslagDokumentData.getRelasjonsRolle().equals(RelasjonsRolleType.MORA.getKode()));
+        assertThat(avslagDokumentData.getvilkårTyper()).hasSize(1);
+        assertThat(avslagDokumentData.getvilkårTyper()).containsExactly("FP_VK_3");
+        assertThat(avslagDokumentData.felles.getFritekst().equals(avslagsfritekst));
+
+    }
+
+    @Test
+    void mapAvslagsårsakerBrev_mapper_riktig() {
+
+        String avslagsårsak = avslagEngangsstønadDokumentDataMapper.mapAvslagsårsakerBrev(Avslagsårsak.SØKER_ER_MEDMOR);
+
+        assertThat(avslagsårsak).isEqualTo("IKKE_ALENEOMSORG");
+    }
+
+     @Test
+    void mapVilkårTIlBrev_skal_mappe_riktig_vilkår_string() {
+        List<Vilkår> vilkårFraBehandling = List.of(new Vilkår(VilkårType.FØDSELSVILKÅRET_MOR));
+        Behandling fbbehandling = opprettBehandling(Avslagsårsak.SØKER_ER_MEDMOR, null, null);
+
+        List<String> vilkårTilBrev = avslagEngangsstønadDokumentDataMapper.utledVilkårTilBrev(vilkårFraBehandling, Avslagsårsak.SØKER_ER_MEDMOR, fbbehandling);
+
+        assertThat(vilkårTilBrev).hasSize(1);
+        assertThat(vilkårTilBrev.get(0)).isEqualTo("FPVK1_4");
+    }
+
+    @Test
+    void utledRelasjonsRolle_skal_utlede_riktig_rolle() {
+        FagsakBackend fagsak = opprettFagsak(RelasjonsRolleType.SAMBOER);
+        String rolle = avslagEngangsstønadDokumentDataMapper.utledRelasjonsRolle(fagsak);
+
+        assertThat(rolle).isEqualTo(RelasjonsRolleType.MEDMOR.getKode());
+    }
+    private Behandling opprettBehandling(Avslagsårsak avslagsårsak, String avslagsfritekst, FagsakBackend fagsak) {
+        var behandlingresultat = Behandlingsresultat.builder()
+                .medAvslagsårsak(avslagsårsak)
+                .medBehandlingResultatType(BehandlingResultatType.AVSLÅTT);
+
+        if (avslagsfritekst != null) {
+            behandlingresultat.medAvslagarsakFritekst(avslagsfritekst);
+        }
+        var behandlingBuilder = Behandling.builder().medId(AvslagEngangsstønadDokumentDataMapperTest.ID)
+                .medBehandlingType(BehandlingType.FØRSTEGANGSSØKNAD)
+                .medBehandlingsresultat(behandlingresultat.build())
+                .medFagsakBackend(fagsak);
+
+        if(fagsak != null) {
+            behandlingBuilder.medFagsakBackend(fagsak);
+        }
+
+        return behandlingBuilder.build();
+    }
+
+    private FagsakBackend opprettFagsak(RelasjonsRolleType relasjonsRolleType) {
+        return FagsakBackend.ny()
+                .medAktørId(AKTØR_ID)
+                .medSaksnummer("123456")
+                .medBrukerRolle(relasjonsRolleType)
+                .build();
+    }
+}

213d2e8a6c5c6768fc47c9468f4140da133fdff3
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2021-02-28 22:13:15 +0100

    NOJIRA saksnummer blir string og forenkle soknad

--- domenetjenester/fpsak/src/main/java/no/nav/foreldrepenger/fpsak/dto/fagsak/FagsakBackendDto.java
+++ domenetjenester/fpsak/src/main/java/no/nav/foreldrepenger/fpsak/dto/fagsak/FagsakBackendDto.java
@@ -3,35 +3,21 @@ package no.nav.foreldrepenger.fpsak.dto.fagsak;
 import no.nav.foreldrepenger.fpsak.dto.kodeverk.KodeDto;
 
 public class FagsakBackendDto {
-    private Long saksnummer;
-    private KodeDto sakstype;
+    //private Long saksnummer;
+    private String saksnummerString;
     private KodeDto relasjonsRolleType;
-    private KodeDto status;
-    private Integer dekningsgrad;
     private String aktoerId;
 
     public FagsakBackendDto() {
         // Injiseres i test
     }
 
-    public Long getSaksnummer() {
-        return saksnummer;
-    }
-
-    public KodeDto getSakstype() {
-        return sakstype;
-    }
-
-    public KodeDto getStatus() {
-        return status;
+    public String getSaksnummerString() {
+        return saksnummerString;
     }
 
     public KodeDto getRelasjonsRolleType() {
         return relasjonsRolleType;
-    }
-
-    public Integer getDekningsgrad() {
-        return dekningsgrad;
     }
 
     public String getAktoerId() {
--- integrasjon/dtomapper/src/main/java/no/nav/foreldrepenger/melding/dtomapper/SøknadDtoMapper.java
+++ integrasjon/dtomapper/src/main/java/no/nav/foreldrepenger/melding/dtomapper/SøknadDtoMapper.java
@@ -1,11 +1,11 @@
 package no.nav.foreldrepenger.melding.dtomapper;
 
-import no.nav.foreldrepenger.fpsak.dto.soknad.SoknadDto;
+import no.nav.foreldrepenger.fpsak.dto.soknad.SoknadBackendDto;
 import no.nav.foreldrepenger.melding.søknad.Søknad;
 import no.nav.foreldrepenger.melding.ytelsefordeling.OppgittRettighet;
 
 public class SøknadDtoMapper {
-    public static Søknad mapSøknadFraDto(SoknadDto dto) {
+    public static Søknad mapSøknadFraDto(SoknadBackendDto dto) {
         boolean aleneomsorgForBarnet = dto.getOppgittRettighet() != null && dto.getOppgittRettighet().isAleneomsorgForBarnet();
         return new Søknad(dto.getMottattDato(), dto.getSoknadsdato(), new OppgittRettighet(aleneomsorgForBarnet));
     }

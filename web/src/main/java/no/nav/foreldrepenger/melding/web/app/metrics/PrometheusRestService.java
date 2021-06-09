a724fe15753a2bd54e59bbbc4c8a863b5b20ef6c
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-09 09:36:12 +0200

    prometheus scrape slik det skal gjøres

--- web/src/main/java/no/nav/foreldrepenger/melding/web/app/metrics/PrometheusRestService.java
+++ web/src/main/java/no/nav/foreldrepenger/melding/web/app/metrics/PrometheusRestService.java
@@ -1,16 +1,11 @@
 package no.nav.foreldrepenger.melding.web.app.metrics;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 
-import io.prometheus.client.CollectorRegistry;
 import io.swagger.v3.oas.annotations.Operation;
+import no.nav.vedtak.log.metrics.MetricsUtil;
 
 @Path("/metrics")
 @ApplicationScoped
@@ -19,13 +14,7 @@ public class PrometheusRestService {
     @GET
     @Operation(tags = "metrics", hidden = true)
     @Path("/prometheus")
-    public Response prometheus() {
-        try (final Writer writer = new StringWriter()) {
-            TextFormatter.write004(writer, CollectorRegistry.defaultRegistry.metricFamilySamples());
-            return Response.ok().encoding("UTF-8").entity(writer.toString()).header("content-type", TextFormatter.CONTENT_TYPE_004).build();
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
+    public String prometheus() {
+        return MetricsUtil.scrape();
     }
 }
-

2fd139c5236b4d9462a1c02945c7db3bf13dd61a
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-09 17:40:56 +0200

    text/plain

--- web/src/main/java/no/nav/foreldrepenger/melding/web/app/metrics/PrometheusRestService.java
+++ web/src/main/java/no/nav/foreldrepenger/melding/web/app/metrics/PrometheusRestService.java
@@ -3,11 +3,14 @@ package no.nav.foreldrepenger.melding.web.app.metrics;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import io.swagger.v3.oas.annotations.Operation;
 import no.nav.vedtak.log.metrics.MetricsUtil;
 
 @Path("/metrics")
+@Produces(MediaType.TEXT_PLAIN)
 @ApplicationScoped
 public class PrometheusRestService {
 

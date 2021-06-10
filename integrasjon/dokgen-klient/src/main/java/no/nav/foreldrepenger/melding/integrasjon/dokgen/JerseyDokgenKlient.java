2a85ab24bc844e4db64791f4fc44ffcc13f2618e
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-10 16:08:49 +0200

    fix

--- integrasjon/dokgen-klient/src/main/java/no/nav/foreldrepenger/melding/integrasjon/dokgen/JerseyDokgenKlient.java
+++ integrasjon/dokgen-klient/src/main/java/no/nav/foreldrepenger/melding/integrasjon/dokgen/JerseyDokgenKlient.java
@@ -7,6 +7,7 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 import java.net.URI;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
@@ -22,6 +23,7 @@ import no.nav.vedtak.felles.integrasjon.rest.jersey.Jersey;
 @Dependent
 public class JerseyDokgenKlient extends AbstractJerseyRestClient implements Dokgen {
     private static final String KODE = "FPFORMIDLING-946543";
+    private static final Set<Språkkode> STØTTEDE_SPRÅK = Set.of(Språkkode.NB, Språkkode.NN, Språkkode.EN);
     private static final String DOKGEN_REST_BASE_URI = "dokgen_rest_base.url";
     private static final String TEMPLATE = "/template/{mal}/template_{språk}/create-pdf-variation";
     private final URI baseUri;
@@ -35,11 +37,15 @@ public class JerseyDokgenKlient extends AbstractJerseyRestClient implements Dokg
     public byte[] genererPdf(String mal, Språkkode språk, Dokumentdata data) {
         return Optional.ofNullable(invoke(client.target(baseUri)
                 .path(TEMPLATE)
-                .resolveTemplates(Map.of("mal", mal, "språk", språk.getKode()))
+                .resolveTemplates(Map.of("mal", mal, "språk", språkkode(språk)))
                 .request(APPLICATION_JSON_TYPE)
                 .buildPost(json(data)), byte[].class))
                 .orElseThrow(() -> new TekniskException(KODE,
                         format("Fikk tomt svar ved kall til dokgen for mal %s og språkkode %s.", mal, språk.getKode())));
+    }
+
+    private static String språkkode(Språkkode kode) {
+        return STØTTEDE_SPRÅK.contains(kode) ? kode.getKode().toLowerCase() : Språkkode.NB.getKode().toLowerCase();
     }
 
     @Override

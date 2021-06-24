69f34416102c0f7b7299042a0c6c025bc7905983
Jan Erik Johnsen <jan.erik.johnsen@nav.no>
2021-06-24 14:50:25 +0200

    FIX: Bedre logging av feilede kall til PDL.

--- domenetjenester/tps/src/main/java/no/nav/foreldrepenger/PersonAdapter.java
+++ domenetjenester/tps/src/main/java/no/nav/foreldrepenger/PersonAdapter.java
@@ -5,34 +5,50 @@ import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import no.nav.foreldrepenger.melding.aktør.Adresseinfo;
 import no.nav.foreldrepenger.melding.aktør.Personinfo;
 import no.nav.foreldrepenger.melding.typer.AktørId;
 import no.nav.foreldrepenger.melding.typer.PersonIdent;
 import no.nav.foreldrepenger.pdl.PersondataTjeneste;
+import no.nav.vedtak.felles.integrasjon.pdl.PdlException;
 
 @ApplicationScoped
 public class PersonAdapter {
 
-    private PersondataTjeneste aktørTjeneste;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonAdapter.class);
+
+    private PersondataTjeneste persondataTjeneste;
 
     public PersonAdapter() {
         // for CDI proxy
     }
 
     @Inject
-    public PersonAdapter(PersondataTjeneste aktørTjeneste) {
-        this.aktørTjeneste = aktørTjeneste;
+    public PersonAdapter(PersondataTjeneste persondataTjeneste) {
+        this.persondataTjeneste = persondataTjeneste;
     }
 
     public Optional<Personinfo> hentBrukerForAktør(AktørId aktørId) {
-        Optional<PersonIdent> funnetFnr = aktørTjeneste.hentPersonIdentForAktørId(aktørId);
-        return funnetFnr.map(pi -> aktørTjeneste.hentPersoninfo(aktørId,pi));
+        try {
+            Optional<PersonIdent> funnetFnr = persondataTjeneste.hentPersonIdentForAktørId(aktørId);
+            return funnetFnr.map(pi -> persondataTjeneste.hentPersoninfo(aktørId,pi));
+        } catch (PdlException pdlException) {
+            LOGGER.error("Fikk feil ved kall til PDL. Detaljer: type={}, cause={}, policy={}", pdlException.getDetails().type(), pdlException.getDetails().cause(), pdlException.getDetails().policy());
+            throw pdlException;
+        }
     }
 
     public Optional<Adresseinfo> hentAdresseinformasjon(AktørId aktørId) {
-        Optional<PersonIdent> funnetFnr = aktørTjeneste.hentPersonIdentForAktørId(aktørId);
-        return funnetFnr.map(pi -> aktørTjeneste.hentAdresseinformasjon(aktørId, pi));
+        try {
+            Optional<PersonIdent> funnetFnr = persondataTjeneste.hentPersonIdentForAktørId(aktørId);
+            return funnetFnr.map(pi -> persondataTjeneste.hentAdresseinformasjon(aktørId, pi));
+        } catch (PdlException pdlException) {
+            LOGGER.error("Fikk feil ved kall til PDL. Detaljer: type={}, cause={}, policy={}", pdlException.getDetails().type(), pdlException.getDetails().cause(), pdlException.getDetails().policy());
+            throw pdlException;
+        }
     }
 
 }

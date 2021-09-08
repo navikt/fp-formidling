3354e01708bf81bfd26086490451190cd848bc4e
Michal J. Sladek <michal.sladek@nav.no>
2021-09-08 10:05:46 +0200

    Fjerner BrevBestillerApplikasjonTjeneste interface.

--- domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/brevbestiller/impl/DokprodBrevproduksjonTjeneste.java
+++ domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/brevbestiller/impl/DokprodBrevproduksjonTjeneste.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

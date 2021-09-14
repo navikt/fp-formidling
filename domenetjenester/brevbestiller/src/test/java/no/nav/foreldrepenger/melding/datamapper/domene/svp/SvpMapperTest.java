aafccaa88bfe21fa9323e7f5723c4d1235af0dac
Jan Erik Johnsen <jan.erik.johnsen@nav.no>
2021-09-14 15:12:48 +0200

    FAGSYSTEM-180644: Lar være å mappe naturalytelse på SVP dersom det forekommer i første periode, da vi bare er interessert i endringer i påfølgende perioder. Byttet om på teksten for bortfaller/tilkommer på naturalytelse slik at det blir riktig.

--- domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/datamapper/domene/svp/SvpMapperTest.java
+++ domenetjenester/brevbestiller/src/test/java/no/nav/foreldrepenger/melding/datamapper/domene/svp/SvpMapperTest.java
@@ -31,18 +31,35 @@ import no.nav.foreldrepenger.melding.virksomhet.Arbeidsgiver;
 
 public class SvpMapperTest {
 
-    private static final LocalDate FOM_DATO = LocalDate.now().minusDays(10);
-    private static final LocalDate TOM_DATO = LocalDate.now().plusDays(10);
+    private static final LocalDate PERIODE1_FOM = LocalDate.now().minusDays(10);
+    private static final LocalDate PERIODE1_TOM = LocalDate.now().minusDays(1);
+    private static final LocalDate PERIODE2_FOM = LocalDate.now();
+    private static final LocalDate PERIODE2_TOM = LocalDate.now().plusDays(10);
     private static final String ARBEIDSGIVER_NAVN = "Arbeidsgiver AS";
     private static final Arbeidsgiver ARBEIDSGIVER = new Arbeidsgiver("1", ARBEIDSGIVER_NAVN);
     private static final long DAGSATS = 1000L;
 
     @Test
+    public void skal_ikke_utlede_naturalytelse_som_starter_med_første_periode() {
+        // Arrange
+        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat(false);
+        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER, false);
+        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(1000), null, of(PeriodeÅrsak.NATURALYTELSE_BORTFALT.getKode()), false);
+
+        // Act
+        Map<String, Object> resultat = SvpMapper.mapPerioder(svpUttaksresultat, beregningsresultatFP, beregningsgrunnlag);
+
+        // Assert
+        Set<Map> naturalytelse = (Set<Map>) resultat.get("naturalytelse");
+        assertThat(naturalytelse).isEmpty();
+    }
+
+    @Test
     public void skal_utlede_at_naturalytelse_bortfaller_når_årsak_er_bortfaller() {
         // Arrange
-        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat();
-        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER);
-        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(1000), null, of(PeriodeÅrsak.NATURALYTELSE_BORTFALT.getKode()));
+        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat(true);
+        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER, true);
+        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(1000), null, of(PeriodeÅrsak.NATURALYTELSE_BORTFALT.getKode()), true);
 
         // Act
         Map<String, Object> resultat = SvpMapper.mapPerioder(svpUttaksresultat, beregningsresultatFP, beregningsgrunnlag);
@@ -55,15 +72,15 @@ public class SvpMapperTest {
         assertThat(naturalytelse.stream().findFirst().get().get(SvpMapper.TILKOMMER)).isNull();
         assertThat(((String)naturalytelse.stream().findFirst().get().get("arbeidsgiverNavn"))).isEqualTo(ARBEIDSGIVER_NAVN);
         assertThat(((long)naturalytelse.stream().findFirst().get().get("nyDagsats"))).isEqualTo(DAGSATS);
-        assertThat(((Dato)naturalytelse.stream().findFirst().get().get("endringsDato"))).isEqualTo(Dato.medFormatering(FOM_DATO));
+        assertThat(((Dato)naturalytelse.stream().findFirst().get().get("endringsDato"))).isEqualTo(Dato.medFormatering(PERIODE2_FOM));
     }
 
     @Test
     public void skal_utlede_at_naturalytelse_tilkommer_når_årsak_er_tilkommer() {
         // Arrange
-        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat();
-        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER);
-        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, null, BigDecimal.valueOf(1000), of(PeriodeÅrsak.NATURALYTELSE_TILKOMMER.getKode()));
+        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat(true);
+        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER, true);
+        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, null, BigDecimal.valueOf(1000), of(PeriodeÅrsak.NATURALYTELSE_TILKOMMER.getKode()), true);
 
         // Act
         Map<String, Object> resultat = SvpMapper.mapPerioder(svpUttaksresultat, beregningsresultatFP, beregningsgrunnlag);
@@ -79,9 +96,9 @@ public class SvpMapperTest {
     @Test
     public void skal_utlede_at_naturalytelse_bortfaller_når_årsak_ikke_er_angitt_og_bare_bortfaller_er_angitt() {
         // Arrange
-        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat();
-        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER);
-        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(1000), null, of());
+        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat(true);
+        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER, true);
+        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(1000), null, of(), true);
 
         // Act
         Map<String, Object> resultat = SvpMapper.mapPerioder(svpUttaksresultat, beregningsresultatFP, beregningsgrunnlag);
@@ -97,9 +114,9 @@ public class SvpMapperTest {
     @Test
     public void skal_utlede_at_naturalytelse_tilkommer_når_årsak_ikke_er_angitt_og_bare_tilkommer_er_angitt() {
         // Arrange
-        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat();
-        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER);
-        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, null, BigDecimal.valueOf(1000), of());
+        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat(true);
+        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER, true);
+        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, null, BigDecimal.valueOf(1000), of(), true);
 
         // Act
         Map<String, Object> resultat = SvpMapper.mapPerioder(svpUttaksresultat, beregningsresultatFP, beregningsgrunnlag);
@@ -115,9 +132,9 @@ public class SvpMapperTest {
     @Test
     public void skal_utlede_at_naturalytelse_bortfaller_når_årsak_ikke_er_angitt_og_bortfaller_er_større_enn_tilkommer() {
         // Arrange
-        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat();
-        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER);
-        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(1000), BigDecimal.valueOf(500), of());
+        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat(true);
+        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER, true);
+        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(1000), BigDecimal.valueOf(500), of(), true);
 
         // Act
         Map<String, Object> resultat = SvpMapper.mapPerioder(svpUttaksresultat, beregningsresultatFP, beregningsgrunnlag);
@@ -133,9 +150,9 @@ public class SvpMapperTest {
     @Test
     public void skal_utlede_at_naturalytelse_tilkommer_når_årsak_ikke_er_angitt_og_bortfaller_er_lik_tilkommer() {
         // Arrange
-        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat();
-        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER);
-        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), of());
+        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat(true);
+        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER, true);
+        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(1000), BigDecimal.valueOf(1000), of(), true);
 
         // Act
         Map<String, Object> resultat = SvpMapper.mapPerioder(svpUttaksresultat, beregningsresultatFP, beregningsgrunnlag);
@@ -151,9 +168,9 @@ public class SvpMapperTest {
     @Test
     public void skal_utlede_at_naturalytelse_tilkommer_når_årsak_ikke_er_angitt_og_bortfaller_er_mindre_enn_tilkommer() {
         // Arrange
-        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat();
-        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER);
-        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(500), BigDecimal.valueOf(1000), of());
+        SvpUttaksresultat svpUttaksresultat = getSvpUttaksresultat(true);
+        BeregningsresultatFP beregningsresultatFP = getBeregningsresultatFP(ARBEIDSGIVER, true);
+        Beregningsgrunnlag beregningsgrunnlag = getBeregningsgrunnlag(ARBEIDSGIVER, BigDecimal.valueOf(500), BigDecimal.valueOf(1000), of(), true);
 
         // Act
         Map<String, Object> resultat = SvpMapper.mapPerioder(svpUttaksresultat, beregningsresultatFP, beregningsgrunnlag);
@@ -166,14 +183,19 @@ public class SvpMapperTest {
         assertThat(((boolean)naturalytelse.stream().findFirst().get().get(SvpMapper.TILKOMMER))).isTrue();
     }
 
-    private SvpUttaksresultat getSvpUttaksresultat() {
-        SvpUttakResultatPeriode uttakPeriode = SvpUttakResultatPeriode.Builder.ny()
-                .medTidsperiode(DatoIntervall.fraOgMedTilOgMed(FOM_DATO, TOM_DATO))
+    private SvpUttaksresultat getSvpUttaksresultat(boolean inkluderePeriode2) {
+        SvpUttakResultatPeriode uttakPeriode1 = SvpUttakResultatPeriode.Builder.ny()
+                .medTidsperiode(DatoIntervall.fraOgMedTilOgMed(PERIODE1_FOM, PERIODE1_TOM))
+                .medArbeidsgiverNavn(ARBEIDSGIVER_NAVN)
+                .medPeriodeResultatType(PeriodeResultatType.INNVILGET)
+                .build();
+        SvpUttakResultatPeriode uttakPeriode2 = SvpUttakResultatPeriode.Builder.ny()
+                .medTidsperiode(DatoIntervall.fraOgMedTilOgMed(PERIODE2_FOM, PERIODE2_TOM))
                 .medArbeidsgiverNavn(ARBEIDSGIVER_NAVN)
                 .medPeriodeResultatType(PeriodeResultatType.INNVILGET)
                 .build();
         SvpUttakResultatArbeidsforhold svpUttakResultatArbeidsforhold = SvpUttakResultatArbeidsforhold.Builder.ny()
-                .leggTilPeriode(uttakPeriode)
+                .leggTilPerioder(inkluderePeriode2 ? of(uttakPeriode1, uttakPeriode2) : of(uttakPeriode1))
                 .build();
         SvpUttaksresultat svpUttaksresultat = SvpUttaksresultat.Builder.ny()
                 .medUttakResultatArbeidsforhold(svpUttakResultatArbeidsforhold)
@@ -181,37 +203,45 @@ public class SvpMapperTest {
         return svpUttaksresultat;
     }
 
-    private BeregningsresultatFP getBeregningsresultatFP(Arbeidsgiver arbeidsgiver) {
+    private BeregningsresultatFP getBeregningsresultatFP(Arbeidsgiver arbeidsgiver, boolean inkluderePeriode2) {
         BeregningsresultatAndel andel = BeregningsresultatAndel.ny()
                 .medArbeidsgiver(arbeidsgiver)
                 .medAktivitetStatus(AktivitetStatus.ARBEIDSTAKER)
                 .build();
-        BeregningsresultatPeriode resultatPeriode = BeregningsresultatPeriode.ny()
-                .medPeriode(DatoIntervall.fraOgMedTilOgMed(FOM_DATO, TOM_DATO))
+        BeregningsresultatPeriode resultatPeriode1 = BeregningsresultatPeriode.ny()
+                .medPeriode(DatoIntervall.fraOgMedTilOgMed(PERIODE1_FOM, PERIODE1_TOM))
                 .medDagsats(DAGSATS)
                 .medBeregningsresultatAndel(of(andel))
                 .build();
-        BeregningsresultatFP beregningsresultatFP = BeregningsresultatFP.ny()
-                .leggTilBeregningsresultatPerioder(of(resultatPeriode))
+        BeregningsresultatPeriode resultatPeriode2 = BeregningsresultatPeriode.ny()
+                .medPeriode(DatoIntervall.fraOgMedTilOgMed(PERIODE2_FOM, PERIODE2_TOM))
+                .medDagsats(DAGSATS)
+                .medBeregningsresultatAndel(of(andel))
                 .build();
-        return beregningsresultatFP;
+        return BeregningsresultatFP.ny()
+                .leggTilBeregningsresultatPerioder(inkluderePeriode2 ? of(resultatPeriode1, resultatPeriode2) : of(resultatPeriode1))
+                .build();
     }
 
-    private Beregningsgrunnlag getBeregningsgrunnlag(Arbeidsgiver arbeidsgiver, BigDecimal naturalytelseBortfaller, BigDecimal naturalytelseTilkommer, List<String> periodeÅrsaker) {
+    private Beregningsgrunnlag getBeregningsgrunnlag(Arbeidsgiver arbeidsgiver, BigDecimal naturalytelseBortfaller, BigDecimal naturalytelseTilkommer, List<String> periodeÅrsaker, boolean inkluderePeriode2) {
         BGAndelArbeidsforhold bgAndelArbeidsforhold = new BGAndelArbeidsforhold(arbeidsgiver, ArbeidsforholdRef.ref("1"), naturalytelseBortfaller, naturalytelseTilkommer);
         BeregningsgrunnlagPrStatusOgAndel beregningsgrunnlagPrStatusOgAndel = BeregningsgrunnlagPrStatusOgAndel.ny()
                 .medBgAndelArbeidsforhold(bgAndelArbeidsforhold)
                 .medAktivitetStatus(AktivitetStatus.ARBEIDSTAKER)
                 .build();
-        BeregningsgrunnlagPeriode grunnlagPeriode = BeregningsgrunnlagPeriode.ny()
-                .medPeriode(DatoIntervall.fraOgMedTilOgMed(FOM_DATO, TOM_DATO))
+        BeregningsgrunnlagPeriode grunnlagPeriode1 = BeregningsgrunnlagPeriode.ny()
+                .medPeriode(DatoIntervall.fraOgMedTilOgMed(PERIODE1_FOM, PERIODE1_TOM))
                 .medBeregningsgrunnlagPrStatusOgAndelList(of(beregningsgrunnlagPrStatusOgAndel))
                 .medperiodeÅrsaker(periodeÅrsaker)
                 .medDagsats(DAGSATS)
                 .build();
-        Beregningsgrunnlag beregningsgrunnlag = Beregningsgrunnlag.ny()
-                .leggTilBeregningsgrunnlagPeriode(grunnlagPeriode)
+        BeregningsgrunnlagPeriode grunnlagPeriode2 = BeregningsgrunnlagPeriode.ny()
+                .medPeriode(DatoIntervall.fraOgMedTilOgMed(PERIODE2_FOM, PERIODE2_TOM))
+                .medBeregningsgrunnlagPrStatusOgAndelList(of(beregningsgrunnlagPrStatusOgAndel))
+                .medperiodeÅrsaker(periodeÅrsaker)
+                .medDagsats(DAGSATS)
                 .build();
-        return beregningsgrunnlag;
+        Beregningsgrunnlag.Builder builder = Beregningsgrunnlag.ny().leggTilBeregningsgrunnlagPeriode(grunnlagPeriode1);
+        return inkluderePeriode2 ? builder.leggTilBeregningsgrunnlagPeriode(grunnlagPeriode2).build() : builder.build();
     }
 }

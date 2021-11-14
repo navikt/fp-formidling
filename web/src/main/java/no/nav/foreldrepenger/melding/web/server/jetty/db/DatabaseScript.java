b7400b7e246220800a6790e52ce6b5af59cb50ed
Michal J. Sladek <michal.sladek@nav.no>
2021-11-14 23:36:06 +0100

    Forenkler flyway oppsett.

--- web/src/main/java/no/nav/foreldrepenger/melding/web/server/jetty/db/DatabaseScript.java
+++ web/src/main/java/no/nav/foreldrepenger/melding/web/server/jetty/db/DatabaseScript.java
@@ -4,21 +4,20 @@ import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.FlywayException;
-import org.flywaydb.core.api.configuration.ClassicConfiguration;
 
 public class DatabaseScript {
 
     private static final String LOCATION = "classpath:/db/migration/";
 
     public static void migrate(final DataSource dataSource, String initSql, boolean clean) {
-        ClassicConfiguration conf = new ClassicConfiguration();
-        conf.setDataSource(dataSource);
-        conf.setLocationsAsStrings(LOCATION);
-        conf.setBaselineOnMigrate(true);
+        var flywayKonfig = Flyway.configure()
+                .dataSource(dataSource)
+                .locations(LOCATION)
+                .baselineOnMigrate(true);
         if (initSql != null) {
-            conf.setInitSql(initSql);
+            flywayKonfig.initSql(initSql);
         }
-        Flyway flyway = new Flyway(conf);
+        var flyway = flywayKonfig.load();
         try {
             flyway.migrate();
         } catch (FlywayException fwe) {

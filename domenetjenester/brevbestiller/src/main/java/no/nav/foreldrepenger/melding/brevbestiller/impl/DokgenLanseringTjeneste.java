1f1520f150b239c099623dbd5ceebcf8ae63f0d5
Michal J. Sladek <michal.sladek@nav.no>
2021-12-16 09:39:31 +0100

    TFP-4762: Lanserer etterlys inntektsmelding.

--- domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/brevbestiller/impl/DokgenLanseringTjeneste.java
+++ domenetjenester/brevbestiller/src/main/java/no/nav/foreldrepenger/melding/brevbestiller/impl/DokgenLanseringTjeneste.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.melding.brevbestiller.impl;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -41,7 +42,8 @@ public class DokgenLanseringTjeneste {
             DokumentMalType.SVANGERSKAPSPENGER_INNVILGELSE,
             DokumentMalType.FRITEKSTBREV,
             DokumentMalType.ANKE_OMGJORT,
-            DokumentMalType.ANKE_OPPHEVET);
+            DokumentMalType.ANKE_OPPHEVET,
+            DokumentMalType.ETTERLYS_INNTEKTSMELDING);
     private static final Set<DokumentMalType> DOKGEN_MALER_DEV = Set.of(
             DokumentMalType.ENGANGSSTØNAD_INNVILGELSE,
             DokumentMalType.ENGANGSSTØNAD_AVSLAG,
@@ -76,35 +78,43 @@ public class DokgenLanseringTjeneste {
             DokumentMalType.VARSEL_OM_REVURDERING_DOK,
             DokumentMalType.FORLENGET_SAKSBEHANDLINGSTID_DOK,
             DokumentMalType.FORLENGET_SAKSBEHANDLINGSTID_MEDL_DOK,
-            DokumentMalType.ETTERLYS_INNTEKTSMELDING);
+            DokumentMalType.ETTERLYS_INNTEKTSMELDING_FRITEKST);
     private static final Set<DokumentMalType> SKJULTE_MANUELLE_MALER_DEV = Set.of(
             DokumentMalType.INNHENTE_OPPLYSNINGER_DOK,
             DokumentMalType.VARSEL_OM_REVURDERING_DOK,
             DokumentMalType.FORLENGET_SAKSBEHANDLINGSTID_DOK,
             DokumentMalType.FORLENGET_SAKSBEHANDLINGSTID_MEDL_DOK,
             DokumentMalType.ETTERLYS_INNTEKTSMELDING_FRITEKST);
-    private static final Map<DokumentMalType, DokumentMalType> OVERSTYRE_MAL_DEV = Map.of(
-            DokumentMalType.VARSEL_OM_REVURDERING_DOK, DokumentMalType.VARSEL_OM_REVURDERING,
-            DokumentMalType.INFO_OM_HENLEGGELSE_DOK, DokumentMalType.INFO_OM_HENLEGGELSE,
-            DokumentMalType.INNSYN_SVAR_DOK, DokumentMalType.INNSYN_SVAR,
-            DokumentMalType.KLAGE_HJEMSENDT_FRITEKST, DokumentMalType.KLAGE_HJEMSENDT,
-            DokumentMalType.KLAGE_OMGJORT_FRITEKST, DokumentMalType.KLAGE_OMGJORT,
-            DokumentMalType.KLAGE_OVERSENDT_FRITEKST, DokumentMalType.KLAGE_OVERSENDT,
-            DokumentMalType.KLAGE_STADFESTET_FRITEKST, DokumentMalType.KLAGE_STADFESTET,
-            DokumentMalType.FRITEKSTBREV_DOK, DokumentMalType.FRITEKSTBREV,
-            DokumentMalType.ANKE_OMGJORT_FRITEKST, DokumentMalType.ANKE_OMGJORT,
-            DokumentMalType.ANKE_OPPHEVET_FRITEKST, DokumentMalType.ANKE_OPPHEVET);
-    private static final Map<DokumentMalType, DokumentMalType> OVERSTYRE_MAL_PROD = Map.of(
-            DokumentMalType.VARSEL_OM_REVURDERING_DOK, DokumentMalType.VARSEL_OM_REVURDERING,
-            DokumentMalType.INFO_OM_HENLEGGELSE_DOK, DokumentMalType.INFO_OM_HENLEGGELSE,
-            DokumentMalType.INNSYN_SVAR_DOK, DokumentMalType.INNSYN_SVAR,
-            DokumentMalType.KLAGE_HJEMSENDT_FRITEKST, DokumentMalType.KLAGE_HJEMSENDT,
-            DokumentMalType.KLAGE_OMGJORT_FRITEKST, DokumentMalType.KLAGE_OMGJORT,
-            DokumentMalType.KLAGE_OVERSENDT_FRITEKST, DokumentMalType.KLAGE_OVERSENDT,
-            DokumentMalType.KLAGE_STADFESTET_FRITEKST, DokumentMalType.KLAGE_STADFESTET,
-            DokumentMalType.FRITEKSTBREV_DOK, DokumentMalType.FRITEKSTBREV,
-            DokumentMalType.ANKE_OMGJORT_FRITEKST, DokumentMalType.ANKE_OMGJORT,
-            DokumentMalType.ANKE_OPPHEVET_FRITEKST, DokumentMalType.ANKE_OPPHEVET);
+
+    private static final Map<DokumentMalType, DokumentMalType> OVERSTYRE_MAL_DEV = new HashMap<>();
+    static {
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.VARSEL_OM_REVURDERING_DOK, DokumentMalType.VARSEL_OM_REVURDERING);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.INFO_OM_HENLEGGELSE_DOK, DokumentMalType.INFO_OM_HENLEGGELSE);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.INNSYN_SVAR_DOK, DokumentMalType.INNSYN_SVAR);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.KLAGE_HJEMSENDT_FRITEKST, DokumentMalType.KLAGE_HJEMSENDT);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.KLAGE_OMGJORT_FRITEKST, DokumentMalType.KLAGE_OMGJORT);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.KLAGE_OVERSENDT_FRITEKST, DokumentMalType.KLAGE_OVERSENDT);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.KLAGE_STADFESTET_FRITEKST, DokumentMalType.KLAGE_STADFESTET);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.FRITEKSTBREV_DOK, DokumentMalType.FRITEKSTBREV);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.ANKE_OMGJORT_FRITEKST, DokumentMalType.ANKE_OMGJORT);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.ANKE_OPPHEVET_FRITEKST, DokumentMalType.ANKE_OPPHEVET);
+        OVERSTYRE_MAL_DEV.put(DokumentMalType.ETTERLYS_INNTEKTSMELDING_FRITEKST, DokumentMalType.ETTERLYS_INNTEKTSMELDING);
+    }
+
+    private static final Map<DokumentMalType, DokumentMalType> OVERSTYRE_MAL_PROD = new HashMap<>();
+    static {
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.VARSEL_OM_REVURDERING_DOK, DokumentMalType.VARSEL_OM_REVURDERING);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.INFO_OM_HENLEGGELSE_DOK, DokumentMalType.INFO_OM_HENLEGGELSE);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.INNSYN_SVAR_DOK, DokumentMalType.INNSYN_SVAR);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.KLAGE_HJEMSENDT_FRITEKST, DokumentMalType.KLAGE_HJEMSENDT);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.KLAGE_OMGJORT_FRITEKST, DokumentMalType.KLAGE_OMGJORT);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.KLAGE_OVERSENDT_FRITEKST, DokumentMalType.KLAGE_OVERSENDT);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.KLAGE_STADFESTET_FRITEKST, DokumentMalType.KLAGE_STADFESTET);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.FRITEKSTBREV_DOK, DokumentMalType.FRITEKSTBREV);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.ANKE_OMGJORT_FRITEKST, DokumentMalType.ANKE_OMGJORT);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.ANKE_OPPHEVET_FRITEKST, DokumentMalType.ANKE_OPPHEVET);
+        OVERSTYRE_MAL_PROD.put(DokumentMalType.ETTERLYS_INNTEKTSMELDING_FRITEKST, DokumentMalType.ETTERLYS_INNTEKTSMELDING);
+    }
 
     private static final Set<DokumentMalType> GENERERING_AV_JSON = Set.of(
             DokumentMalType.SVANGERSKAPSPENGER_OPPHØR,

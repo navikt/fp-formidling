9d118eaad7749db6229ea2abc5d92be78d2e22c7
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-02-15 12:25:59 +0100

    Fikser prosess task logging. (#1769)

--- brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/task/BrevTaskProperties.java
+++ brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/task/BrevTaskProperties.java
@@ -1,9 +1,11 @@
 package no.nav.foreldrepenger.fpformidling.brevproduksjon.task;
 
+import no.nav.vedtak.felles.prosesstask.api.CommonTaskProperties;
+
 public final class BrevTaskProperties {
 
     public static final String HENDELSE_ID = "hendelseId";
-    public static final String BEHANDLING_UUID = "behandlingUUId";
+    public static final String BEHANDLING_UUID = CommonTaskProperties.BEHANDLING_UUID;
     public static final String JOURNALPOST_ID = "journalpostId";
 
     private BrevTaskProperties() {

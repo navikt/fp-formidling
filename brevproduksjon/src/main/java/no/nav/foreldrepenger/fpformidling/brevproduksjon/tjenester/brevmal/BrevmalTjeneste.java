9a2de205024f8618ae33f4d961ae47f222bca9c1
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-03-01 11:17:05 +0100

    Siste fase overgang til jsonvalue (#1808)

--- brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/tjenester/brevmal/BrevmalTjeneste.java
+++ brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/tjenester/brevmal/BrevmalTjeneste.java
@@ -14,7 +14,6 @@ import no.nav.foreldrepenger.fpformidling.behandling.Behandling;
 import no.nav.foreldrepenger.fpformidling.brevproduksjon.tjenester.DomeneobjektProvider;
 import no.nav.foreldrepenger.fpformidling.kodeverk.kodeverdi.DokumentMalRestriksjon;
 import no.nav.foreldrepenger.fpformidling.kodeverk.kodeverdi.DokumentMalType;
-import no.nav.foreldrepenger.kontrakter.formidling.kodeverk.EnumDokumentMalRestriksjon;
 import no.nav.foreldrepenger.kontrakter.formidling.v1.BrevmalDto;
 
 @ApplicationScoped
@@ -94,29 +93,9 @@ public class BrevmalTjeneste {
         List<BrevmalDto> brevmalDtoList = new ArrayList<>(dmtList.size());
         for (DokumentMalType dmt : dmtList) {
             boolean tilgjengelig = sjekkDokumentTilgjengelig.sjekkOmTilgjengelig(behandling, dmt);
-            brevmalDtoList.add(new BrevmalDto(dmt.getKode(), dmt.getNavn(),
-                    mapKontraktDokumentMalRestriksjon(dmt.getDokumentmalRestriksjon()),
-                    mapKontraktEnumDokumentMalRestriksjon(dmt.getDokumentmalRestriksjon()),
-                    tilgjengelig));
+            brevmalDtoList.add(new BrevmalDto(dmt.getKode(), dmt.getNavn(), tilgjengelig));
         }
         return brevmalDtoList;
     }
 
-    private no.nav.foreldrepenger.kontrakter.formidling.kodeverk.DokumentMalRestriksjon mapKontraktDokumentMalRestriksjon(DokumentMalRestriksjon dokumentMalRestriksjon) {
-        return  switch (dokumentMalRestriksjon) {
-            case REVURDERING -> no.nav.foreldrepenger.kontrakter.formidling.kodeverk.DokumentMalRestriksjon.REVURDERING;
-            case ÅPEN_BEHANDLING -> no.nav.foreldrepenger.kontrakter.formidling.kodeverk.DokumentMalRestriksjon.ÅPEN_BEHANDLING;
-            case ÅPEN_BEHANDLING_IKKE_SENDT -> no.nav.foreldrepenger.kontrakter.formidling.kodeverk.DokumentMalRestriksjon.ÅPEN_BEHANDLING_IKKE_SENDT;
-            default -> no.nav.foreldrepenger.kontrakter.formidling.kodeverk.DokumentMalRestriksjon.INGEN;
-        };
-    }
-
-    private EnumDokumentMalRestriksjon mapKontraktEnumDokumentMalRestriksjon(DokumentMalRestriksjon dokumentMalRestriksjon) {
-        return  switch (dokumentMalRestriksjon) {
-            case REVURDERING -> EnumDokumentMalRestriksjon.REVURDERING;
-            case ÅPEN_BEHANDLING -> EnumDokumentMalRestriksjon.ÅPEN_BEHANDLING;
-            case ÅPEN_BEHANDLING_IKKE_SENDT -> EnumDokumentMalRestriksjon.ÅPEN_BEHANDLING_IKKE_SENDT;
-            default -> EnumDokumentMalRestriksjon.INGEN;
-        };
-    }
 }

053c07248c4c1851e1d2105a37f1a74bc769b311
Anders Palfi <anderspalfi@gmail.com>
2022-03-09 10:23:31 +0100

    TFP4067 ignorerer ny stønadskonto

--- integrasjon/fpsak/src/main/java/no/nav/foreldrepenger/fpsak/mapper/StønadskontoDtoMapper.java
+++ integrasjon/fpsak/src/main/java/no/nav/foreldrepenger/fpsak/mapper/StønadskontoDtoMapper.java
@@ -4,6 +4,7 @@ import java.util.stream.Collectors;
 
 import no.nav.foreldrepenger.fpformidling.uttak.Saldoer;
 import no.nav.foreldrepenger.fpformidling.uttak.Stønadskonto;
+import no.nav.foreldrepenger.fpformidling.uttak.StønadskontoType;
 import no.nav.foreldrepenger.fpsak.dto.uttak.saldo.SaldoerDto;
 import no.nav.foreldrepenger.fpsak.dto.uttak.saldo.StønadskontoDto;
 
@@ -12,11 +13,12 @@ public class StønadskontoDtoMapper {
     public static Saldoer mapSaldoerFraDto(SaldoerDto dto) {
         return new Saldoer(dto.getStonadskontoer().values()
                 .stream()
+                .filter(k -> !StønadskontoType.UTEN_AKTIVITETSKRAV.equals(k.getStonadskontotype()))
                 .map(StønadskontoDtoMapper::mapStønadskontoFradto)
                 .collect(Collectors.toSet()), dto.getTapteDagerFpff());
     }
 
-    static Stønadskonto mapStønadskontoFradto(StønadskontoDto dto) {
+    private static Stønadskonto mapStønadskontoFradto(StønadskontoDto dto) {
         return new Stønadskonto(dto.getMaxDager(), dto.getStonadskontotype(), dto.getSaldo(), mapPrematurDager(dto), mapFlerbarnsdager(dto));
     }
 

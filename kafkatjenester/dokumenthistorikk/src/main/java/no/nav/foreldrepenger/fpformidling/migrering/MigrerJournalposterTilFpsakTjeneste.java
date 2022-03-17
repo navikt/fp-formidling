ea4d5cbc2e13fd073884e98273ccc1b1e2728eb9
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-03-17 18:02:14 +0100

    Oppretter en migrering task (#1854)

--- kafkatjenester/dokumenthistorikk/src/main/java/no/nav/foreldrepenger/fpformidling/migrering/MigrerJournalposterTilFpsakTjeneste.java
+++ kafkatjenester/dokumenthistorikk/src/main/java/no/nav/foreldrepenger/fpformidling/migrering/MigrerJournalposterTilFpsakTjeneste.java
@@ -1,79 +1,33 @@
 package no.nav.foreldrepenger.fpformidling.migrering;
 
-import java.io.IOException;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
-import no.nav.foreldrepenger.fpformidling.historikk.DokumentHistorikkinnslag;
-import no.nav.foreldrepenger.fpformidling.historikk.HistorikkRepository;
-import no.nav.foreldrepenger.kontrakter.formidling.v1.DokumentProdusertDto;
+import no.nav.foreldrepenger.fpformidling.historikk.task.MigrerHistorikkTask;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 
 @ApplicationScoped
 public class MigrerJournalposterTilFpsakTjeneste {
 
-    private static final Logger log = LoggerFactory.getLogger(MigrerJournalposterTilFpsakTjeneste.class);
-
-    private static final ObjectMapper mapper = getObjectMapper();
-
-    private DokumentHistorikkinnslagProducer meldingProducer;
-    private HistorikkRepository historikkRepository;
+    private ProsessTaskTjeneste taskTjeneste;
 
     public MigrerJournalposterTilFpsakTjeneste() {
         //CDI
     }
 
     @Inject
-    public MigrerJournalposterTilFpsakTjeneste(DokumentHistorikkinnslagProducer dokumentMeldingProducer, HistorikkRepository historikkRepository) {
-        this.meldingProducer = dokumentMeldingProducer;
-        this.historikkRepository = historikkRepository;
+    public MigrerJournalposterTilFpsakTjeneste(ProsessTaskTjeneste taskTjeneste) {
+        this.taskTjeneste = taskTjeneste;
     }
 
     public void migrerHistorikk() {
-        historikkRepository.all().stream().map(this::toDto).forEach(this::send);
+        opprettTask();
     }
 
-    private DokumentProdusertDto toDto(DokumentHistorikkinnslag dokumentHistorikkinnslag) {
-        return new DokumentProdusertDto(
-                dokumentHistorikkinnslag.getBehandlingUuid(),
-                dokumentHistorikkinnslag.getHistorikkUuuid(),
-                dokumentHistorikkinnslag.getDokumentMalType().getKode(),
-                dokumentHistorikkinnslag.getJournalpostId().getVerdi(),
-                dokumentHistorikkinnslag.getDokumentId()
-        );
-    }
-
-    private void send(DokumentProdusertDto historikk) {
-        meldingProducer.sendJson(serialiser(historikk));
-    }
-
-    private String serialiser(DokumentProdusertDto historikk) {
-        try {
-            return mapper.writeValueAsString(historikk);
-        } catch (IOException e) {
-            log.error("Klarte ikke å serialisere historikkinnslag");
-            return null;
-        }
-    }
-
-    private static ObjectMapper getObjectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new Jdk8Module());
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return mapper;
+    private void opprettTask() {
+        var data = ProsessTaskData.forProsessTask(MigrerHistorikkTask.class);
+        taskTjeneste.lagre(data);
     }
 
 }

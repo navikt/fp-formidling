36e6fc294a0b3af3dfc3df8c98b1a14fbd05283e
Michal J. Sladek <michal.sladek@nav.no>
2022-06-09 10:34:01 +0200

    IgNot null sjekk.

--- brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/integrasjon/dokdist/dto/DistribuerJournalpostRequest.java
+++ brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/integrasjon/dokdist/dto/DistribuerJournalpostRequest.java
@@ -1,15 +1,17 @@
 package no.nav.foreldrepenger.fpformidling.integrasjon.dokdist.dto;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import no.nav.foreldrepenger.fpformidling.kodeverk.kodeverdi.Fagsystem;
 import no.nav.foreldrepenger.fpformidling.typer.JournalpostId;
 
-public record DistribuerJournalpostRequest(String journalpostId,
-                                           String batchId,
-                                           String bestillendeFagsystem,
-                                           String dokumentProdApp,
+public record DistribuerJournalpostRequest(@NotNull String journalpostId,
+                                           @NotNull String batchId,
+                                           @NotNull String bestillendeFagsystem,
+                                           @NotNull String dokumentProdApp,
                                            @JsonInclude(Include.NON_NULL) Distribusjonstype distribusjonstype,
                                            @JsonInclude(Include.NON_NULL) Distribusjonstidspunkt distribusjonstidspunkt) {
     public DistribuerJournalpostRequest(JournalpostId id,

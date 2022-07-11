ad3e4fa9cf8d70d16901d2b7df0b4f6e67e002af
Michal J. Sladek <michal.sladek@nav.no>
2022-07-11 09:17:01 +0200

    Sonar

--- brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/integrasjon/http/JavaHttpKlient.java
+++ brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/integrasjon/http/JavaHttpKlient.java
@@ -10,7 +10,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.Optional;
 
 import no.nav.vedtak.exception.TekniskException;
 import no.nav.vedtak.mapper.json.DefaultJsonMapper;

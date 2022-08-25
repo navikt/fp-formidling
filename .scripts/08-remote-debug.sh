6f82d1cfdfb99507b5c448220b80bff0f4598ed8
Michal J. Sladek <michal.sladek@nav.no>
2022-08-25 13:35:35 +0200

    Endrer debug commando

--- .scripts/08-remote-debug.sh
+++ .scripts/08-remote-debug.sh
@@ -3,5 +3,5 @@
 # Enables remote debugging on port 5005 if the application is running in a development cluster
 
 if [ "$NAIS_CLUSTER_NAME" = "dev-fss" ] || [ "$NAIS_CLUSTER_NAME" = "dev-gcp" ]; then
-  export JAVA_OPTS="${JAVA_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"
+  export JAVA_OPTS="${JAVA_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
 fi

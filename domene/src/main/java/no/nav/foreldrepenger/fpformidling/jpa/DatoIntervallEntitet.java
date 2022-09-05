b8f1c5307635165e1aca8fd9e87bcb3df2f823d3
Anders Palfi <anderspalfi@gmail.com>
2022-09-05 10:01:43 +0200

    Forenklet oppslag av arbeidsgiver, fjerner ubrukt kode (#2038)

--- domene/src/main/java/no/nav/foreldrepenger/fpformidling/jpa/DatoIntervallEntitet.java
+++ domene/src/main/java/no/nav/foreldrepenger/fpformidling/jpa/DatoIntervallEntitet.java
@@ -40,18 +40,6 @@ public class DatoIntervallEntitet extends AbstractLocalDateInterval {
         return new DatoIntervallEntitet(fomDato, tomDato);
     }
 
-    public static DatoIntervallEntitet fraOgMed(LocalDate fomDato) {
-        return new DatoIntervallEntitet(fomDato, TIDENES_ENDE);
-    }
-
-    public static DatoIntervallEntitet fraOgMedPlusArbeidsdager(LocalDate fom, int antallArbeidsdager) {
-        return DatoIntervallEntitet.fraOgMedTilOgMed(fom, finnTomDato(fom, antallArbeidsdager));
-    }
-
-    public static DatoIntervallEntitet tilOgMedMinusArbeidsdager(LocalDate tom, int antallArbeidsdager) {
-        return DatoIntervallEntitet.fraOgMedTilOgMed(finnFomDato(tom, antallArbeidsdager), tom);
-    }
-
     @Override
     public LocalDate getFomDato() {
         return fomDato;
@@ -60,11 +48,6 @@ public class DatoIntervallEntitet extends AbstractLocalDateInterval {
     @Override
     public LocalDate getTomDato() {
         return tomDato;
-    }
-
-    @Override
-    protected DatoIntervallEntitet lagNyPeriode(LocalDate fomDato, LocalDate tomDato) {
-        return fraOgMedTilOgMed(fomDato, tomDato);
     }
 
     @Override

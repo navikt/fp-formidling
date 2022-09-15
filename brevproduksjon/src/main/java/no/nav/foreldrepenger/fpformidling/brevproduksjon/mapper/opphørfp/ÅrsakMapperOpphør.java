0f1e71353375279b45e3631a8e42d14eac554e77
Anders Palfi <anderspalfi@gmail.com>
2022-09-15 15:19:28 +0200

    Trådsafe mapping av lovreferanser

--- brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/mapper/opphørfp/ÅrsakMapperOpphør.java
+++ brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/mapper/opphørfp/ÅrsakMapperOpphør.java
@@ -5,10 +5,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeSet;
-
-import javax.enterprise.context.ApplicationScoped;
 
 import no.nav.foreldrepenger.fpformidling.behandling.Behandlingsresultat;
 import no.nav.foreldrepenger.fpformidling.behandling.ÅrsakMedLovReferanse;
@@ -22,45 +19,46 @@ import no.nav.foreldrepenger.fpformidling.uttak.UttakResultatPeriode;
 import no.nav.foreldrepenger.fpformidling.uttak.kodeliste.PeriodeResultatÅrsak;
 import no.nav.foreldrepenger.fpformidling.vilkår.Avslagsårsak;
 
-@ApplicationScoped
-public class ÅrsakMapperOpphør {
+public final class ÅrsakMapperOpphør {
 
-    private static Set<String> lovReferanser;
+    private ÅrsakMapperOpphør() {
+    }
 
     public static Tuple<List<String>, String> mapÅrsakslisteOgLovhjemmelFra(Behandlingsresultat behandlingsresultat,
-                                                                      ForeldrepengerUttak foreldrepengerUttak) {
-        lovReferanser = new TreeSet<>(new LovhjemmelComparator());
+                                                                            ForeldrepengerUttak foreldrepengerUttak) {
+        var lovReferanser = new TreeSet<>(new LovhjemmelComparator());
         List<String> årsaksliste = new ArrayList<>();
-        årsaksliste.addAll(årsakerFra(behandlingsresultat, foreldrepengerUttak));
+        årsaksliste.addAll(årsakerFra(behandlingsresultat, foreldrepengerUttak, lovReferanser));
 
         return new Tuple<>(årsaksliste, FellesMapper.formaterLovhjemlerUttak(lovReferanser));
     }
 
     private static Collection<String> årsakerFra(Behandlingsresultat behandlingsresultat,
-                                                 ForeldrepengerUttak foreldrepengerUttak) {
+                                                 ForeldrepengerUttak foreldrepengerUttak,
+                                                 TreeSet<String> lovReferanser) {
         Map<String, String> avslagårsaker = new HashMap<>();
 
         Avslagsårsak avslagsårsak = behandlingsresultat.getAvslagsårsak();
         if (avslagsårsak != null) {
             String avslagKode = avslagsårsak.getKode();
-            avslagårsaker.put(avslagKode, årsaktypeFra(avslagsårsak));
+            avslagårsaker.put(avslagKode, årsaktypeFra(avslagsårsak, lovReferanser));
         }
         for (UttakResultatPeriode periode : foreldrepengerUttak.perioder()) {
             PeriodeResultatÅrsak periodeResultatÅrsak = periode.getPeriodeResultatÅrsak();
             if (PeriodeResultatType.AVSLÅTT.equals(periode.getPeriodeResultatType()) && periodeResultatÅrsak != null) {
                 String avslagKode = periodeResultatÅrsak.getKode();
-                avslagårsaker.put(avslagKode, årsaktypeFra(periodeResultatÅrsak));
+                avslagårsaker.put(avslagKode, årsaktypeFra(periodeResultatÅrsak, lovReferanser));
             }
         }
         return avslagårsaker.values();
     }
 
-    private static String årsaktypeFra(PeriodeResultatÅrsak periodeResultatÅrsak) {
+    private static String årsaktypeFra(PeriodeResultatÅrsak periodeResultatÅrsak, TreeSet<String> lovReferanser) {
         lovReferanser.addAll(LovhjemmelUtil.hentLovhjemlerFraJson(periodeResultatÅrsak, "FP"));
         return periodeResultatÅrsak.getKode();
     }
 
-    private static String årsaktypeFra(ÅrsakMedLovReferanse årsakKode) { //NOSONAR - denne er i bruk...
+    private static String årsaktypeFra(ÅrsakMedLovReferanse årsakKode, TreeSet<String> lovReferanser) {
         lovReferanser.addAll(LovhjemmelUtil.hentLovhjemlerFraJson(årsakKode, "FP"));
         return årsakKode.getKode();
     }

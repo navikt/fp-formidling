d7e41061ef68a211916b66c68ced6ed25be78238
Michal J. Sladek <michal.sladek@nav.no>
2022-11-01 21:56:38 +0100

    Enda mer logging.

--- web/src/main/java/no/nav/foreldrepenger/fpformidling/web/server/jetty/JettyServer.java
+++ web/src/main/java/no/nav/foreldrepenger/fpformidling/web/server/jetty/JettyServer.java
@@ -183,7 +183,7 @@ public class JettyServer {
         ctx.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed", "false");
         //ctx.setAttribute(WEBINF_JAR_PATTERN, "^.*jersey-.*\\.jar$|^.*felles-.*\\.jar$");
 
-        ctx.setAttribute(CONTAINER_JAR_PATTERN, ".*/target/classes/|^.*jersey-.*\\.jar$|^.*felles-.*\\.jar$|^app\\.jar$");
+        ctx.setAttribute(CONTAINER_JAR_PATTERN, "^.*/target/classes/|^.*jersey-.*\\.jar$|^.*felles-.*\\.jar$|^.*/app\\.jar$");
 
         ctx.addEventListener(new org.jboss.weld.environment.servlet.BeanManagerResourceBindingListener());
         ctx.addEventListener(new org.jboss.weld.environment.servlet.Listener());

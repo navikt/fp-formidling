e2bc088e732bce02d878757150292542bcb3badd
Michal J. Sladek <michal.sladek@nav.no>
2023-06-02 11:22:17 +0200

    Bump libs + fix oppgaver

--- lokalutvikling/postgres-init/fpformidling.sql
+++ lokalutvikling/postgres-init/fpformidling.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE fpformidling_unit;
 CREATE USER fpformidling_unit WITH PASSWORD 'fpformidling_unit';
-GRANT ALL PRIVILEGES ON DATABASE fpformidling_unit TO fpformidling_unit;
+GRANT ALL ON DATABASE fpformidling_unit TO fpformidling_unit;
+ALTER DATABASE fpformidling_unit SET timezone TO 'Europe/Oslo';
+ALTER DATABASE fpformidling_unit OWNER TO fpformidling_unit;
 
 CREATE DATABASE fpformidling;
 CREATE USER fpformidling WITH PASSWORD 'fpformidling';
-GRANT ALL PRIVILEGES ON DATABASE fpformidling TO fpformidling;
+GRANT ALL ON DATABASE fpformidling TO fpformidling;
+ALTER DATABASE fpformidling SET timezone TO 'Europe/Oslo';
+ALTER DATABASE fpformidling OWNER TO fpformidling;

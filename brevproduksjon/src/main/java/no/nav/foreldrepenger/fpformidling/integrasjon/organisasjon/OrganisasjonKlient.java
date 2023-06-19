bb2b481c1f6e421685509016136bbded5c3a1916
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-06-19 10:12:27 +0200

    Bruk ereg-services v2

--- brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/integrasjon/organisasjon/OrganisasjonKlient.java
+++ brevproduksjon/src/main/java/no/nav/foreldrepenger/fpformidling/integrasjon/organisasjon/OrganisasjonKlient.java
@@ -7,7 +7,7 @@ import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 import javax.enterprise.context.Dependent;
 
 @Dependent
-@RestClientConfig(tokenConfig = TokenFlow.STS_CC, endpointProperty = "organisasjon.rs.url", endpointDefault = "https://modapp.adeo.no/ereg/api/v1/organisasjon")
+@RestClientConfig(tokenConfig = TokenFlow.NO_AUTH_NEEDED, endpointProperty = "organisasjon.rs.url", endpointDefault = "https://ereg-services.intern.nav.no/api/v2/organisasjon")
 public class OrganisasjonKlient extends AbstractOrganisasjonKlient {
 
     public OrganisasjonKlient() {

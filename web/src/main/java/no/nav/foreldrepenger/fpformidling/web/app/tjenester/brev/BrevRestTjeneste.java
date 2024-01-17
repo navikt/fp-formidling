7088cb8ff749eed0a4e1a9d27c1f24c71786124f
Michal J. Sladek <michal.sladek@nav.no>
2024-01-17 16:12:05 +0100

    Slå på brevproduksjon.

--- web/src/main/java/no/nav/foreldrepenger/fpformidling/web/app/tjenester/brev/BrevRestTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/fpformidling/web/app/tjenester/brev/BrevRestTjeneste.java
@@ -87,10 +87,9 @@ public class BrevRestTjeneste {
     @BeskyttetRessurs(actionType = ActionType.CREATE, resourceType = ResourceType.FAGSAK)
     @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
     public Response bestillDokument(@Parameter(description = "Inneholder kode til brevmal og bestillingsdetaljer.") @TilpassetAbacAttributt(supplierClass = BestillingSupplier.class) @Valid DokumentbestillingV2Dto dokumentbestillingDto) { // NOSONAR
-        throw new IllegalStateException("Tjenesten er midlertidig utilgjengelig.");
-        //var hendelse = DokumentHendelseDtoMapper.mapFra(dokumentbestillingDto);
-        //dokumentHendelseTjeneste.validerUnikOgLagre(hendelse).ifPresent(this::opprettBestillBrevTask);
-        //return Response.ok().build();
+        var hendelse = DokumentHendelseDtoMapper.mapFra(dokumentbestillingDto);
+        dokumentHendelseTjeneste.validerUnikOgLagre(hendelse).ifPresent(this::opprettBestillBrevTask);
+        return Response.ok().build();
     }
 
     private void opprettBestillBrevTask(DokumentHendelse dokumentHendelse) {

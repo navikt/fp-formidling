e05221dd89dcc21dfa8107f98c48477a5afd2f5a
Anders Palfi <anderspalfi@gmail.com>
2024-06-26 08:30:06 +0200

    TFP-5678 utvider også med antall dager flerbarnsdager for 80 deknings… (#2542)
    TFP-5678 utvider også med antall dager flerbarnsdager for 80 dekningsgrad, som ikke lenger går opp i hele antall uker

--- src/test/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/mapper/innvilgelsefp/StønadskontoMapperTest.java
+++ src/test/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/mapper/innvilgelsefp/StønadskontoMapperTest.java
@@ -1,16 +1,17 @@
 package no.nav.foreldrepenger.fpformidling.brevproduksjon.mapper.innvilgelsefp;
 
-import no.nav.foreldrepenger.fpformidling.brevproduksjon.mapper.innvilgelsefp.StønadskontoMapper;
+import static no.nav.foreldrepenger.fpformidling.brevproduksjon.mapper.innvilgelsefp.StønadskontoMapper.finnFlerbarnsdagerUtvidetDagerHvisFinnes;
+import static no.nav.foreldrepenger.fpformidling.brevproduksjon.mapper.innvilgelsefp.StønadskontoMapper.finnFlerbarnsdagerUtvidetUkerHvisFinnes;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
 import no.nav.foreldrepenger.fpformidling.domene.personopplysning.RelasjonsRolleType;
 import no.nav.foreldrepenger.fpformidling.domene.uttak.fp.SaldoVisningStønadskontoType;
 import no.nav.foreldrepenger.fpformidling.domene.uttak.fp.Saldoer;
 import no.nav.foreldrepenger.fpformidling.domene.uttak.fp.Stønadskonto;
-
-import org.junit.jupiter.api.Test;
-
-import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class StønadskontoMapperTest {
 
@@ -40,5 +41,29 @@ class StønadskontoMapperTest {
         var stønadskontoer = Set.of(new Stønadskonto(10, SaldoVisningStønadskontoType.FEDREKVOTE, 5, 0, 0));
         var dager = StønadskontoMapper.finnDisponibleDager(new Saldoer(stønadskontoer, 0), RelasjonsRolleType.FARA);
         assertThat(dager).isEqualTo(5);
+    }
+
+    @Test
+    void flerbarnsdager_ingen() {
+        var stønadskontoer = Set.of(new Stønadskonto(100, SaldoVisningStønadskontoType.FELLESPERIODE, 5, 0, 0));
+        var saldoer = new Saldoer(stønadskontoer, 0);
+        assertThat(finnFlerbarnsdagerUtvidetUkerHvisFinnes(saldoer)).isZero();
+        assertThat(finnFlerbarnsdagerUtvidetDagerHvisFinnes(saldoer)).isZero();
+    }
+
+    @Test
+    void flerbarnsdager_hele_uker() {
+        var stønadskontoer = Set.of(new Stønadskonto(100, SaldoVisningStønadskontoType.FELLESPERIODE, 5, 0, 20));
+        var saldoer = new Saldoer(stønadskontoer, 0);
+        assertThat(finnFlerbarnsdagerUtvidetUkerHvisFinnes(saldoer)).isEqualTo(4);
+        assertThat(finnFlerbarnsdagerUtvidetDagerHvisFinnes(saldoer)).isZero();
+    }
+
+    @Test
+    void flerbarnsdager_uker_og_dager() {
+        var stønadskontoer = Set.of(new Stønadskonto(100, SaldoVisningStønadskontoType.FELLESPERIODE, 5, 0, 32));
+        var saldoer = new Saldoer(stønadskontoer, 0);
+        assertThat(finnFlerbarnsdagerUtvidetUkerHvisFinnes(saldoer)).isEqualTo(6);
+        assertThat(finnFlerbarnsdagerUtvidetDagerHvisFinnes(saldoer)).isEqualTo(2);
     }
 }

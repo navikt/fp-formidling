91cd27db47820302210fbe8bb850ea0342586445
Michal J. Sladek <michal.sladek@nav.no>
2024-12-05 07:38:38 +0100

    test: forenkler testen

--- src/test/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/tjenester/historikk/DokumentKvitteringTjenesteTest.java
+++ src/test/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/tjenester/historikk/DokumentKvitteringTjenesteTest.java
@@ -1,14 +1,11 @@
 package no.nav.foreldrepenger.fpformidling.brevproduksjon.tjenester.historikk;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 
 import java.util.UUID;
 
-import no.nav.foreldrepenger.kontrakter.formidling.v3.DokumentKvitteringDto;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -18,6 +15,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import no.nav.foreldrepenger.fpformidling.integrasjon.fpsak.Behandlinger;
+import no.nav.foreldrepenger.kontrakter.formidling.v3.DokumentKvitteringDto;
 
 @ExtendWith(MockitoExtension.class)
 class DokumentKvitteringTjenesteTest {
@@ -28,16 +26,13 @@ class DokumentKvitteringTjenesteTest {
     private Behandlinger fpsakKlient;
 
     @Captor
-    ArgumentCaptor<DokumentKvitteringDto> kvitteringCaptor;
-
-    @BeforeEach
-    void setup() {
-        historikkTjeneste = new DokumentKvitteringTjeneste(fpsakKlient);
-        lenient().doNothing().when(fpsakKlient).kvitterDokument(Mockito.any());
-    }
+    private ArgumentCaptor<DokumentKvitteringDto> kvitteringCaptor;
 
     @Test
     void publiserHistorikk() {
+        historikkTjeneste = new DokumentKvitteringTjeneste(fpsakKlient);
+        doNothing().when(fpsakKlient).kvitterDokument(Mockito.any());
+
         var forventetSaksnummer = "saksnummer";
         var forventetJp = "123";
         var forventetDokumentId = "123";

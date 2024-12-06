71074ce947e1128447f4e05f495261f19f8e8542
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-12-06 13:36:09 +0100

    Bruk setBehandlingUuid

--- src/main/java/no/nav/foreldrepenger/fpformidling/server/abac/AppPdpRequestBuilderImpl.java
+++ src/main/java/no/nav/foreldrepenger/fpformidling/server/abac/AppPdpRequestBuilderImpl.java
@@ -1,11 +1,13 @@
 package no.nav.foreldrepenger.fpformidling.server.abac;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
+import no.nav.vedtak.log.mdc.MdcExtendedLogContext;
 import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
 import no.nav.vedtak.sikkerhet.abac.PdpRequestBuilder;
 import no.nav.vedtak.sikkerhet.abac.StandardAbacAttributtType;
@@ -18,6 +20,8 @@ import no.nav.vedtak.sikkerhet.abac.pdp.AppRessursData;
 @Dependent
 public class AppPdpRequestBuilderImpl implements PdpRequestBuilder {
 
+    private static final MdcExtendedLogContext MDC_EXTENDED_LOG_CONTEXT = MdcExtendedLogContext.getContext("prosess");
+
     public static final String ABAC_DOMAIN = "foreldrepenger";
 
     private final PipRestKlient pipRestKlient;
@@ -29,6 +33,8 @@ public class AppPdpRequestBuilderImpl implements PdpRequestBuilder {
 
     @Override
     public AppRessursData lagAppRessursData(AbacDataAttributter dataAttributter) {
+        Set<String> saksnummer = new HashSet<>(dataAttributter.getVerdier(StandardAbacAttributtType.SAKSNUMMER));
+        MDC_EXTENDED_LOG_CONTEXT.add("fagsak", saksnummer.stream().findFirst().orElse(null));
         Set<UUID> uuids = dataAttributter.getVerdier(StandardAbacAttributtType.BEHANDLING_UUID);
         var behandlingUuids = uuids.stream().distinct().toList();
         if (behandlingUuids.size() > 1) {
@@ -37,6 +43,7 @@ public class AppPdpRequestBuilderImpl implements PdpRequestBuilder {
 
         var appRessursData = AppRessursData.builder();
         behandlingUuids.stream().findFirst().ifPresent(b -> {
+            MDC_EXTENDED_LOG_CONTEXT.add("behandling", b);
             var dto = pipRestKlient.hentPipdataForBehandling(b);
             appRessursData.leggTilAbacAktørIdSet(dto.aktørIder());
             Optional.ofNullable(dto.fagsakStatus()).ifPresent(appRessursData::medFagsakStatus);

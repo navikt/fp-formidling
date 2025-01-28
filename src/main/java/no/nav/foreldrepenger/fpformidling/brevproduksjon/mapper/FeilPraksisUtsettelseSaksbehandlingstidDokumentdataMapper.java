e893e1f84d56fdd7b852100efd6da352314b84f5
Anders Palfi <anders.palfi@nav.no>
2025-01-28 11:19:31 +0100

    TFP-5942 brev for forlenget saksehandlingstid feilpraksis utsettelse

---
+++ src/main/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/mapper/FeilPraksisUtsettelseSaksbehandlingstidDokumentdataMapper.java
@@ -0,0 +1,34 @@
+package no.nav.foreldrepenger.fpformidling.brevproduksjon.mapper;
+
+import static no.nav.foreldrepenger.fpformidling.typer.Dato.formaterDato;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import no.nav.foreldrepenger.fpformidling.brevproduksjon.mapper.felles.BrevMapperUtil;
+import no.nav.foreldrepenger.fpformidling.brevproduksjon.mapper.felles.DokumentdataMapper;
+import no.nav.foreldrepenger.fpformidling.domene.behandling.Behandling;
+import no.nav.foreldrepenger.fpformidling.domene.dokumentdata.DokumentFelles;
+import no.nav.foreldrepenger.fpformidling.domene.dokumentdata.DokumentMalTypeRef;
+import no.nav.foreldrepenger.fpformidling.domene.hendelser.DokumentHendelse;
+import no.nav.foreldrepenger.fpformidling.integrasjon.dokgen.dto.FeilPraksisUtsettelseInfobrevDokumentdata;
+import no.nav.foreldrepenger.fpformidling.kodeverk.kodeverdi.DokumentMalType;
+
+@ApplicationScoped
+@DokumentMalTypeRef(DokumentMalType.FORELDREPENGER_FEIL_PRAKSIS_UTSETTELSE_FORLENGET_SAKSBEHANDLINGSTID)
+public class FeilPraksisUtsettelseSaksbehandlingstidDokumentdataMapper implements DokumentdataMapper {
+
+    @Override
+    public String getTemplateNavn() {
+        return "foreldrepenger-feil-praksis-utsettelse-forlenget-saksbehandlingstid";
+    }
+
+    @Override
+    public FeilPraksisUtsettelseInfobrevDokumentdata mapTilDokumentdata(DokumentFelles dokumentFelles, DokumentHendelse hendelse, Behandling behandling, boolean erUtkast) {
+        var fellesBuilder = BrevMapperUtil.opprettFellesBuilder(dokumentFelles, behandling, erUtkast);
+        fellesBuilder.medBrevDato(getBrevDato(dokumentFelles, behandling));
+        return new FeilPraksisUtsettelseInfobrevDokumentdata(fellesBuilder.build());
+    }
+
+    private static String getBrevDato(DokumentFelles dokumentFelles, Behandling behandling) {
+        return dokumentFelles.getDokumentDato() != null ? formaterDato(dokumentFelles.getDokumentDato(), behandling.getSpråkkode()) : null;
+    }
+}
--- src/main/java/no/nav/foreldrepenger/fpformidling/tjenester/DokumentHendelseDtoMapper.java
+++ src/main/java/no/nav/foreldrepenger/fpformidling/tjenester/DokumentHendelseDtoMapper.java
@@ -47,6 +47,7 @@ public final class DokumentHendelseDtoMapper {
             case FORELDREPENGER_ANNULLERT -> no.nav.foreldrepenger.fpformidling.typer.DokumentMal.FORELDREPENGER_ANNULLERT;
             case FORELDREPENGER_INFO_TIL_ANNEN_FORELDER -> no.nav.foreldrepenger.fpformidling.typer.DokumentMal.FORELDREPENGER_INFOBREV_TIL_ANNEN_FORELDER;
             case FORELDREPENGER_FEIL_PRAKSIS_UTSETTELSE_INFOBREV -> no.nav.foreldrepenger.fpformidling.typer.DokumentMal.FORELDREPENGER_FEIL_PRAKSIS_UTSETTELSE_INFOBREV;
+            case FORELDREPENGER_FEIL_PRAKSIS_UTSETTELSE_FORLENGET_SAKSBEHANDLINGSTID -> no.nav.foreldrepenger.fpformidling.typer.DokumentMal.FORELDREPENGER_FEIL_PRAKSIS_UTSETTELSE_FORLENGET_SAKSBEHANDLINGSTID;
             case SVANGERSKAPSPENGER_INNVILGELSE -> no.nav.foreldrepenger.fpformidling.typer.DokumentMal.SVANGERSKAPSPENGER_INNVILGELSE;
             case SVANGERSKAPSPENGER_OPPHØR -> no.nav.foreldrepenger.fpformidling.typer.DokumentMal.SVANGERSKAPSPENGER_OPPHØR;
             case SVANGERSKAPSPENGER_AVSLAG -> no.nav.foreldrepenger.fpformidling.typer.DokumentMal.SVANGERSKAPSPENGER_AVSLAG;

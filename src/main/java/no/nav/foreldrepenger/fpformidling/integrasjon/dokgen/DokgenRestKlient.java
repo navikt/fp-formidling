bcddca01e2fb2d24eec6edae93c8887ae43c274e
espenwaaga <espen.waaga@nav.no>
2025-03-10 10:56:20 +0100

    Generering av html skjer nå i et eget endepunkt

--- src/main/java/no/nav/foreldrepenger/fpformidling/integrasjon/dokgen/DokgenRestKlient.java
+++ src/main/java/no/nav/foreldrepenger/fpformidling/integrasjon/dokgen/DokgenRestKlient.java
@@ -46,16 +46,15 @@ public class DokgenRestKlient implements Dokgen {
     @Override
     public String genererHtml(String maltype, Språkkode språkkode, Dokumentdata dokumentdata) {
         var templatePath = String.format("/template/%s/template_%s", maltype.toLowerCase(), getSpråkkode(språkkode));
-        var endpoint = UriBuilder.fromUri(restConfig.endpoint()).path(templatePath).path("/create-pdf-variation").build();
+        var endpoint = UriBuilder.fromUri(restConfig.endpoint()).path(templatePath).path("/create-html-variation-of-pdf").build();
         var request = RestRequest.newPOSTJson(dokumentdata, endpoint, restConfig);
-        request.setAndReplaceHeader(HttpHeaders.ACCEPT, MediaType.TEXT_HTML);
-        var pdf = restClient.sendReturnResponseString(request).body();
+        var html = restClient.sendReturnResponseString(request).body();
 
-        if (pdf == null || pdf.isEmpty()) {
+        if (html == null || html.isEmpty()) {
             throw new TekniskException("FPFORMIDLING-946543",
-                    String.format("Fikk tomt svar ved kall til dokgen for mal %s og språkkode %s.", maltype, språkkode));
+                    String.format("Fikk tomt svar (html) ved kall til dokgen for mal %s og språkkode %s.", maltype, språkkode));
         }
-        return pdf;
+        return html;
     }
 
     private String getSpråkkode(Språkkode språkkode) {

b1d94475820df1a5d6cc7f930c71e4914b83cfb8
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-10-04 20:11:04 +0200

    test: validere norsk beløpsformattering

--- src/test/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/mapper/EngangsstønadInnvilgelseDokumentdataMapperTest.java
+++ src/test/java/no/nav/foreldrepenger/fpformidling/brevproduksjon/mapper/EngangsstønadInnvilgelseDokumentdataMapperTest.java
@@ -8,8 +8,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -172,6 +174,6 @@ class EngangsstønadInnvilgelseDokumentdataMapperTest {
     }
 
     private String formaterBeløp(long beløp) {
-        return String.format("%,d", beløp);
+        return NumberFormat.getIntegerInstance(Locale.forLanguageTag("nb-NO")).format(beløp);
     }
 }

bf3cc3d5ef92a3814b957f571710971c4f61fb42
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-01-27 08:05:13 +0100

    TFP-5437 bruk standard JsonMapper overalt (#2712)

--- src/main/java/no/nav/foreldrepenger/fpformidling/konfig/JacksonJsonConfig.java
+++ src/main/java/no/nav/foreldrepenger/fpformidling/konfig/JacksonJsonConfig.java
@@ -1,32 +1,21 @@
 package no.nav.foreldrepenger.fpformidling.konfig;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import jakarta.ws.rs.ext.ContextResolver;
 import jakarta.ws.rs.ext.Provider;
+import no.nav.vedtak.mapper.json.DefaultJsonMapper;
 
 
 @Provider
 public class JacksonJsonConfig implements ContextResolver<ObjectMapper> {
 
-    private final ObjectMapper objectMapper;
-
-    public JacksonJsonConfig() {
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new Jdk8Module());
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    }
-
+    private static final JsonMapper JM = DefaultJsonMapper.getJsonMapper();
 
     @Override
     public ObjectMapper getContext(Class<?> type) {
-        return objectMapper;
+        return JM;
     }
 
 }

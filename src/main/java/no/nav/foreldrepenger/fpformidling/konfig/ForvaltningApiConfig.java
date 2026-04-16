ce3d3c16c35fb501eee0092d904fcfc8ec573d58
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-04-16 11:25:08 +0200

    chore: fjern utdatert mapper-setting

--- src/main/java/no/nav/foreldrepenger/fpformidling/konfig/ForvaltningApiConfig.java
+++ src/main/java/no/nav/foreldrepenger/fpformidling/konfig/ForvaltningApiConfig.java
@@ -7,19 +7,17 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
-import no.nav.vedtak.server.rest.ForvaltningAuthorizationFilter;
-
 import org.glassfish.jersey.server.ServerProperties;
 
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 import no.nav.foreldrepenger.fpformidling.tjenester.forvaltning.ForvaltningRestTjeneste;
 import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.vedtak.felles.prosesstask.rest.ProsessTaskRestTjeneste;
 import no.nav.vedtak.openapi.OpenApiUtils;
+import no.nav.vedtak.server.rest.ForvaltningAuthorizationFilter;
 import no.nav.vedtak.server.rest.FpRestJackson2Feature;
-import no.nav.vedtak.server.rest.GeneralRestExceptionMapper;
 
 @ApplicationPath(ForvaltningApiConfig.API_URL)
 public class ForvaltningApiConfig extends Application {
@@ -29,7 +27,6 @@ public class ForvaltningApiConfig extends Application {
     public static final String API_URL = "/forvaltning/api";
 
     public ForvaltningApiConfig() {
-        GeneralRestExceptionMapper.setBrukerRettetApplikasjon(false);
         var contextPath = ENV.getProperty("context.path", "/fpformidling");
         OpenApiUtils.setupOpenApi("FPFORMIDLING Forvaltning - Foreldrepenger, engangsstønad og svangerskapspenger",
             contextPath, getAllClasses(), this);

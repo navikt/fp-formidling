c93dc9f9b91de652414a27e131e81f85ba3d4530
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-04-21 21:29:46 +0200

    Chore: Slett ferdige tasks fortløpende

--- src/main/java/no/nav/foreldrepenger/fpformidling/felles/prosesstask/SlettGamleBestillingerTask.java
+++ src/main/java/no/nav/foreldrepenger/fpformidling/felles/prosesstask/SlettGamleBestillingerTask.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.fpformidling.felles.prosesstask;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ import no.nav.foreldrepenger.fpformidling.domene.hendelser.HendelseRepository;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 
 @Dependent
 @ProsessTask(value = "vedlikehold.tasks.slettgamle", cronExpression = "0 45 1 * * *", maxFailedRuns = 1)
@@ -18,16 +20,20 @@ public class SlettGamleBestillingerTask implements ProsessTaskHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(SlettGamleBestillingerTask.class);
     private final HendelseRepository hendelseRepository;
+    private final ProsessTaskTjeneste prosessTaskTjeneste;
 
     @Inject
-    public SlettGamleBestillingerTask(HendelseRepository hendelseRepository) {
+    public SlettGamleBestillingerTask(HendelseRepository hendelseRepository, ProsessTaskTjeneste prosessTaskTjeneste) {
         this.hendelseRepository = hendelseRepository;
+        this.prosessTaskTjeneste = prosessTaskTjeneste;
     }
 
     @Override
     public void doTask(ProsessTaskData prosessTaskData) {
-        var bestillingerSlettet = hendelseRepository.slettDokumentHendelserEldreEnn(LocalDate.now().minusMonths(1));
+        var bestillingerSlettet = hendelseRepository.slettDokumentHendelserEldreEnn(LocalDate.now().minusWeeks(4));
         LOG.info("Slettet {} dokumentbestillinger som er over 1 måned gamle.", bestillingerSlettet);
+        var tasksSlettet = prosessTaskTjeneste.slettFerdigeEldreEnn(LocalDateTime.now().minusWeeks(4));
+        LOG.info("Slettet {} prosesstasks som er over 1 måned gamle.", tasksSlettet);
     }
 
 }

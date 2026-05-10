c8a1034dab282eb2c40f9ba978a2f12c46ecde3e
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2026-05-10 16:01:34 +0200

    Chore: Fjerne ubrukt oppsett i lokalutvikling (#2741)

--- lokalutvikling/postgres-init/fpformidling.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE fpformidling;
-CREATE USER fpformidling WITH PASSWORD 'fpformidling';
-GRANT ALL ON DATABASE fpformidling TO fpformidling;
-ALTER DATABASE fpformidling SET timezone TO 'Europe/Oslo';
-ALTER DATABASE fpformidling OWNER TO fpformidling;

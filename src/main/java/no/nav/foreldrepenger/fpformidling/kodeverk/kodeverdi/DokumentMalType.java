29f3a097e4452d7806a6bf3f21e25f7b3405ae52
Anders Palfi <anders.palfi@nav.no>
2026-05-12 13:06:37 +0200

    Chore: Fjerner unødvendig deprecated brevmaler, bruker ikke mot db

--- src/main/java/no/nav/foreldrepenger/fpformidling/kodeverk/kodeverdi/DokumentMalType.java
+++ src/main/java/no/nav/foreldrepenger/fpformidling/kodeverk/kodeverdi/DokumentMalType.java
@@ -32,47 +32,7 @@ public enum DokumentMalType {
     KLAGE_OMGJORT("KGEOMG", "Vedtak om omgjøring av klage"),
     KLAGE_OVERSENDT("KGEOVE", "Klage oversendt til klageinstans"),
     ETTERLYS_INNTEKTSMELDING("ELYSIM", "Etterlys inntektsmelding"),
-    ENDRING_UTBETALING("ENDUTB", "Endring i fordeling av ytelsen"), // Denne er kun teknisk når sbh overstyrer vedtaksbrev pga endring i utbetaling av ytelse. Dette skal på sikt bli en egen mal
-
-    // Disse brevene er utgåtte, men beholdes her grunnet historisk bruk i databasen:
-    @Deprecated FRITEKSTBREV_DOK("FRITKS", "Fritekstbrev"), //NOSONAR
-    @Deprecated ENGANGSSTØNAD_INNVILGELSE_DOK("POSVED", "Positivt vedtaksbrev"), //NOSONAR
-    @Deprecated ENGANGSSTØNAD_AVSLAG_DOK("AVSLAG", "Avslagsbrev"), //NOSONAR
-    @Deprecated FORELDREPENGER_INNVILGELSE_DOK("INNVFP", "Innvilgelsesbrev Foreldrepenger"), //NOSONAR
-    @Deprecated FORELDREPENGER_AVSLAG_DOK("AVSLFP", "Avslagsbrev Foreldrepenger"), //NOSONAR
-    @Deprecated FORELDREPENGER_OPPHØR_DOK("OPPHOR", "Opphør brev"), //NOSONAR
-    @Deprecated FORELDREPENGER_INFOBREV_TIL_ANNEN_FORELDER_DOK("INAFOR", //NOSONAR
-        "Informasjonsbrev til den andre forelderen"), //NOSONAR
-    @Deprecated SVANGERSKAPSPENGER_INNVILGELSE_FRITEKST("INNSVP", //NOSONAR
-        "Innvilgelsesbrev svangerskapspenger"), //NOSONAR
-    @Deprecated INNHENTE_OPPLYSNINGER_DOK("INNHEN", "Innhent dokumentasjon"), //NOSONAR
-    @Deprecated VARSEL_OM_REVURDERING_DOK("REVURD", "Varsel om revurdering"), //NOSONAR
-    @Deprecated INFO_OM_HENLEGGELSE_DOK("HENLEG", "Behandling henlagt"), //NOSONAR
-    @Deprecated INNSYN_SVAR_DOK("INSSKR", "Svar på innsynskrav"), //NOSONAR
-    @Deprecated IKKE_SØKT_DOK("INNTID", "Ikke mottatt søknad"), //NOSONAR
-    @Deprecated INGEN_ENDRING_DOK("UENDRE", "Uendret utfall"), //NOSONAR
-    @Deprecated FORLENGET_SAKSBEHANDLINGSTID_DOK("FORLEN", "Forlenget saksbehandlingstid"), //NOSONAR
-    @Deprecated FORLENGET_SAKSBEHANDLINGSTID_MEDL_DOK("FORLME", //NOSONAR
-        "Forlenget saksbehandlingstid - medlemskap"), //NOSONAR
-    @Deprecated FORLENGET_SAKSBEHANDLINGSTID_TIDLIG_DOK("FORLTS", //NOSONAR
-        "Forlenget saksbehandlingstid - Tidlig søknad"), //NOSONAR
-    @Deprecated KLAGE_AVVIST_DOK("KLAGAV", Constants.VEDTAK_OM_AVVIST_KLAGE), //NOSONAR
-    @Deprecated KLAGE_AVVIST_FRITEKST("KAVVIS", Constants.VEDTAK_OM_AVVIST_KLAGE), //NOSONAR
-    @Deprecated KLAGE_HJEMSENDT_DOK("KLAGNY", "Vedtak opphevet, sendt til ny behandling"), //NOSONAR
-    @Deprecated KLAGE_HJEMSENDT_FRITEKST("KHJEMS", "Klage hjemsendt/opphevet"), //NOSONAR
-    @Deprecated KLAGE_OMGJORT_DOK("VEDMED", "Vedtak om medhold"), //NOSONAR
-    @Deprecated KLAGE_OMGJORT_FRITEKST("KOMGJO", "Vedtak om omgjøring av klage"), //NOSONAR
-    @Deprecated KLAGE_OVERSENDT_DOK("KLAGOV", "Overføring til NAV Klageinstans"), //NOSONAR
-    @Deprecated KLAGE_OVERSENDT_FRITEKST("KOVKLA", "Klage oversendt til klageinstans"), //NOSONAR
-    @Deprecated KLAGE_STADFESTET_DOK("KLAGVE", "Vedtak om stadfestelse"), //NOSONAR
-    @Deprecated KLAGE_STADFESTET_FRITEKST("KSTADF", "Vedtak om stadfestelse i klagesak"), //NOSONAR
-    @Deprecated ANKE_OMGJORT_FRITEKST("VEDOGA", "Vedtak om omgjøring i ankesak"), //NOSONAR
-    @Deprecated ANKE_OPPHEVET_FRITEKST("ANKEBO", "Ankebrev om beslutning om oppheving"), //NOSONAR
-    @Deprecated ETTERLYS_INNTEKTSMELDING_FRITEKST("INNLYS", "Etterlys inntektsmelding"), //NOSONAR
-    @Deprecated ANKE_OMGJORT("ANKOMG", "Vedtak om omgjøring i ankesak"), //NOSONAR
-    @Deprecated ANKE_OPPHEVET("ANKOPP", "Ankebrev om beslutning om oppheving"),  //NOSONAR
-    @Deprecated KLAGE_STADFESTET("KGESTA", "Vedtak om stadfestelse i klagesak"), //NOSONAR
-    @Deprecated KLAGE_HJEMSENDT("KGEHJE", "Klage hjemsendt/opphevet"), //NOSONAR
+    ENDRING_UTBETALING("ENDUTB", "Endring i fordeling av ytelsen"), // Brukes som journalførSom-type, ikke egen brevmal. Gir tittel og kode til Joark ved overstyrt vedtaksbrev
 
     UDEFINERT(Fpsak.STANDARDKODE_UDEFINERT, null);
 

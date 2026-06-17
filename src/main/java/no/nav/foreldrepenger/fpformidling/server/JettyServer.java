a2e4f71b2ba66f39ae2154064377c19faa270f4b
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-06-17 18:41:08 +0200

    TFP-7017 bruker mer felles db-relatert kode, exit JNDI

--- src/main/java/no/nav/foreldrepenger/fpformidling/server/JettyServer.java
+++ src/main/java/no/nav/foreldrepenger/fpformidling/server/JettyServer.java
@@ -3,11 +3,6 @@ package no.nav.foreldrepenger.fpformidling.server;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Properties;
-import java.util.concurrent.TimeUnit;
-
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 import org.eclipse.jetty.ee11.cdi.CdiDecoratingListener;
 import org.eclipse.jetty.ee11.cdi.CdiServletContainerInitializer;
@@ -16,7 +11,6 @@ import org.eclipse.jetty.ee11.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee11.servlet.ServletHolder;
 import org.eclipse.jetty.ee11.servlet.security.ConstraintMapping;
 import org.eclipse.jetty.ee11.servlet.security.ConstraintSecurityHandler;
-import org.eclipse.jetty.plus.jndi.EnvEntry;
 import org.eclipse.jetty.security.Constraint;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.ForwardedRequestCustomizer;
@@ -25,20 +19,19 @@ import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.ContextHandler;
-import org.flywaydb.core.Flyway;
-import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
 import no.nav.foreldrepenger.fpformidling.konfig.ApiConfig;
 import no.nav.foreldrepenger.fpformidling.konfig.ForvaltningApiConfig;
 import no.nav.foreldrepenger.fpformidling.konfig.InternalApiConfig;
 import no.nav.foreldrepenger.konfig.Environment;
+import no.nav.vedtak.felles.jpa.NamingStandard;
+import no.nav.vedtak.felles.jpa.flyway.FlywayUtil;
+import no.nav.vedtak.felles.jpa.jdbc.DataSourceHolder;
+import no.nav.vedtak.felles.jpa.jdbc.DatasourceUtil;
 
 public class JettyServer {
 
@@ -65,7 +58,9 @@ public class JettyServer {
 
     void bootStrap() throws Exception {
         konfigurerLogging();
-        migrer(setupDataSource());
+        var ds = DatasourceUtil.postgresDataSource(ENV.getRequiredProperty("DB_JDBC_URL"), null, null, 16);
+        DataSourceHolder.initialize(ds);
+        FlywayUtil.migrate(ds, NamingStandard.DEFAULT_DS_MIGRATION_CLASSPATH);
         start();
     }
 
@@ -78,41 +73,6 @@ public class JettyServer {
         SLF4JBridgeHandler.install();
     }
 
-
-    private static DataSource setupDataSource() throws NamingException {
-        var dataSource = dataSource();
-        new EnvEntry("jdbc/defaultDS", dataSource);
-        return dataSource;
-    }
-
-    private static DataSource dataSource() {
-        var config = new HikariConfig();
-        config.setJdbcUrl(ENV.getRequiredProperty("DB_JDBC_URL"));
-        config.setConnectionTimeout(TimeUnit.SECONDS.toMillis(2));
-        config.setMinimumIdle(1);
-        config.setMaximumPoolSize(16);
-        config.setInitializationFailTimeout(TimeUnit.SECONDS.toMillis(30));
-        config.setConnectionTestQuery("select 1");
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setAutoCommit(false);
-
-        // optimaliserer inserts for postgres
-        var dsProperties = new Properties();
-        dsProperties.setProperty("reWriteBatchedInserts", "true");
-        dsProperties.setProperty("logServerErrorDetail", "false"); // skrur av batch exceptions som lekker statements i åpen logg
-        config.setDataSourceProperties(dsProperties);
-
-        return new HikariDataSource(config);
-    }
-
-    private static FluentConfiguration flywayConfig(DataSource dataSource) {
-        return Flyway.configure().dataSource(dataSource).locations("classpath:/db/migration/defaultDS").baselineOnMigrate(true);
-    }
-
-    private static void migrer(DataSource dataSource) {
-        var flyway = flywayConfig(dataSource);
-        flyway.load().migrate();
-    }
 
     private void start() throws Exception {
         var server = new Server(getServerPort());
